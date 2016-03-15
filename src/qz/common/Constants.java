c3b1e2cd8dea708531ec0f482c9955d49961ddd5
Tres Finocchiaro <tres.finocchiaro@gmail.com>
2016-03-14 22:25:52 -0400

    Set version RC3

--- src/qz/common/Constants.java
+++ src/qz/common/Constants.java
@@ -12,7 +12,7 @@ public class Constants {
     public static final int OUTPUT_STREAM_SIZE = 20480;
     public static final int BYTE_BUFFER_SIZE = 8192;
     public static final int APPEND_XML = 1;
-    public static final String VERSION = "2.0.0";
+    public static final String VERSION = "2.0.0-RC3";
     public static final int APPEND_RAW = 2;
     public static final int APPEND_IMAGE = 3;
     public static final int APPEND_IMAGE_PS = 4;

695a5fefee4ac8dc405fb46b15fd614f0d2244ae
Berenz <thebrettberenz@gmail.com>
2016-07-22 15:50:22 -0400

    Account for whitespace in media names

--- src/qz/printer/action/PrintPixel.java
+++ src/qz/printer/action/PrintPixel.java
@@ -43,7 +43,7 @@ public abstract class PrintPixel {
         }
         if (pxlOpts.getPrinterTray() != null) {
             for(Media m: supported){
-                if (m.toString().equals(pxlOpts.getPrinterTray())) {
+                if (m.toString().trim().equals(pxlOpts.getPrinterTray())) {
                     attributes.add(m);
                     break;
                 }

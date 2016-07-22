a59d135d1a2e8c3d5addc348c627c872b158f3ff
Tres Finocchiaro <tres.finocchiaro@gmail.com>
2016-07-22 16:03:57 -0400

    Bump version
    Bump version for 2.1.0-RC0
    So that testers know which branch they're running

--- src/qz/common/Constants.java
+++ src/qz/common/Constants.java
@@ -9,7 +9,7 @@ public class Constants {
     public static final String HEXES = "0123456789ABCDEF";
     public static final char[] HEXES_ARRAY = HEXES.toCharArray();
     public static final int BYTE_BUFFER_SIZE = 8192;
-    public static final String VERSION = "2.0.1";
+    public static final String VERSION = "2.1.0-RC0";
 
     /* QZ-Tray Constants */
     public static final String BLOCK_FILE = "blocked";

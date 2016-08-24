500c585bacce1008c2cc258de5a859c5134430d0
Berenz <thebrettberenz@gmail.com>
2016-08-24 17:45:13 -0400

    Shared site manager certificates

---
+++ src/qz/ui/CertificateDisplay.java
@@ -0,0 +1,38 @@
+package qz.ui;
+
+import qz.auth.Certificate;
+
+public class CertificateDisplay {
+
+    private Certificate cert;
+    private boolean local = true;
+
+    public CertificateDisplay(Certificate cert, boolean local) {
+        this.cert = cert;
+        this.local = local;
+    }
+
+    public Certificate getCert() {
+        return cert;
+    }
+
+    public boolean isLocal() {
+        return local;
+    }
+
+    @Override
+    public String toString() {
+        return cert.toString();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Object compareTo = obj;
+        if (obj instanceof CertificateDisplay) {
+            compareTo = ((CertificateDisplay)obj).getCert();
+        }
+
+        //true if the passed object is a matching certificate (either directly or from another CertificateDisplay object)
+        return cert.equals(compareTo);
+    }
+}

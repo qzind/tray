16127c01b837ef1e0d7dfbe0b5fb1d31ad768bf4
Tres Finocchiaro <tres.finocchiaro@gmail.com>
2016-09-20 21:57:25 -0400

    Autostart by default (#107)

--- src/qz/deploy/DeployUtilities.java
+++ src/qz/deploy/DeployUtilities.java
@@ -113,7 +113,7 @@ public abstract class DeployUtilities {
      * @param toggleType Shortcut type, i.e. <code>ToggleType.STARTUP</code> or <code>ToggleType.DESKTOP</code>
      * @return Whether or not the shortcut already exists
      */
-    public boolean hasShortcut(ToggleType toggleType) {
+    private boolean hasShortcut(ToggleType toggleType) {
         boolean hasShortcut = false;
         switch(toggleType) {
             case STARTUP:
@@ -165,7 +165,7 @@ public abstract class DeployUtilities {
      * @return The calculated working path value, or an empty string if one
      * could not be determined
      */
-    static String getParentDirectory(String filePath) {
+    private static String getParentDirectory(String filePath) {
         // Working path should always default to the JARs parent folder
         int lastSlash = filePath.lastIndexOf(File.separator);
         return lastSlash < 0? "":filePath.substring(0, lastSlash);
@@ -199,25 +199,6 @@ public abstract class DeployUtilities {
         } else {
             return new LinuxDeploy();
         }
-    }
-
-    /**
-     * Creates all appropriate parent folders for the file path specified
-     *
-     * @param filePath The file in which to create parent directories for
-     * @return Whether or not the parent folder creation was successful
-     */
-    static boolean createParentFolder(String filePath) {
-        String parentDirectory = getParentDirectory(filePath);
-        File f = new File(parentDirectory);
-        try {
-            f.mkdirs();
-            return f.exists();
-        }
-        catch(SecurityException e) {
-            log.error("Error while creating parent directories for: {}", filePath, e);
-        }
-        return false;
     }
 
     /**
@@ -295,7 +276,8 @@ public abstract class DeployUtilities {
      * @param filePath The full file path to set the execute flag on
      * @return <code>true</code> if successful, <code>false</code> otherwise
      */
-    static boolean setExecutable(String filePath) {
+    @SuppressWarnings("ResultOfMethodCallIgnored")
+    private static boolean setExecutable(String filePath) {
         if (!SystemUtilities.isWindows()) {
             try {
                 File f = new File(filePath);
@@ -314,7 +296,7 @@ public abstract class DeployUtilities {
     /**
      * Gets the path to qz-tray.properties
      */
-    public static String detectPropertiesPath() {
+    private static String detectPropertiesPath() {
         // Use supplied path from IDE or command line
         // i.e  -DsslPropertiesFile=C:\qz-tray.properties
         String override = System.getProperty("sslPropertiesFile");
@@ -326,7 +308,6 @@ public abstract class DeployUtilities {
         String propFile = Constants.PROPS_FILE + ".properties";
 
         // Use relative path based on qz-tray.jar, fix %20
-        // TODO:  Find a better way to fix the unicode chars?
         return fixWhitespaces(getParentDirectory(jarPath) + File.separator + propFile);
     }
 
@@ -356,7 +337,7 @@ public abstract class DeployUtilities {
      * @return A String value representing the absolute path to the currently running
      * jar
      */
-    public static String detectJarPath() {
+    private static String detectJarPath() {
         try {
             return new File(DeployUtilities.class.getProtectionDomain()
                                     .getCodeSource().getLocation().getPath()).getCanonicalPath();
@@ -377,15 +358,6 @@ public abstract class DeployUtilities {
             jarPath = detectJarPath();
         }
         return jarPath;
-    }
-
-    /**
-     * Set the jar path for which we will create a shortcut for
-     *
-     * @param jarPath The full file path of the jar file
-     */
-    public void setJarPath(String jarPath) {
-        this.jarPath = jarPath;
     }
 
     /**

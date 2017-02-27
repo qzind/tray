138c7ce247d48ee5a18aa9a1cabc7bf84b5af14e
mrloop <ewan@mrloop.com>
2017-02-27 12:23:59 +0000

    fixup! Set User-Agent for Requests

--- src/qz/utils/ConnectionUtilities.java
+++ src/qz/utils/ConnectionUtilities.java
@@ -8,12 +8,22 @@ import java.net.URLConnection;
 import qz.common.Constants;
 
 public final class ConnectionUtilities {
-	private ConnectionUtilities() {
-	}
-	
-	public static InputStream getInputStream(String urlString) throws IOException {
-		URLConnection urlConn = new URL(urlString).openConnection();
-        urlConn.setRequestProperty("User-Agent", Constants.HTTP_USER_AGENT);
+    private ConnectionUtilities() {
+    }
+
+    public static InputStream getInputStream(String urlString) throws IOException {
+        URLConnection urlConn = new URL(urlString).openConnection();
+        urlConn.setRequestProperty("User-Agent", userAgent());
         return urlConn.getInputStream();
-	}
+    }
+
+    public static String userAgent() {
+        String.format("%s/%s (%s; %s) Java/%s (%s)",
+            Constants.ABOUT_TITLE.replaceAll("[^a-zA-Z]", ""),
+            Constants.VERSION,
+            System.getProperty("os.name"),
+            System.getProperty("os.version"),
+            System.getProperty("java.version"),
+            System.getProperty("java.vendor"));
+    }
 }

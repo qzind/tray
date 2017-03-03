eeb9edb7b142fcca50ef2cf0c09b293b12e46005
Berenz <thebrettberenz@gmail.com>
2017-03-03 16:43:24 -0500

    Make scale-to-page adjust for changes from web dpi

--- src/qz/printer/action/WebAppModel.java
+++ src/qz/printer/action/WebAppModel.java
@@ -10,10 +10,13 @@ public class WebAppModel {
     private boolean isScaled = true;
 
     public WebAppModel(String source, boolean plainText, double webWidth, double webHeight, boolean isScaled) {
+        //values supplied are at print dpi, scale up to web dpi here
+        double increase = 96d / 72d;
+
         this.source = source;
         this.plainText = plainText;
-        this.webWidth = webWidth;
-        this.webHeight = webHeight;
+        this.webWidth = webWidth * increase;
+        this.webHeight = webHeight * increase;
         this.isScaled = isScaled;
     }
 

fe5944666854227c724cae91c8553e9b176b71ec
Berenz <thebrettberenz@gmail.com>
2017-06-14 11:22:59 -0400

    Fix bad index error with simple print calls

--- js/qz-tray.js
+++ js/qz-tray.js
@@ -917,8 +917,8 @@ var qz = (function() {
          */
         print: function(configs, data) {
             var resumeOnError = false,
-                signatures = null,
-                signaturesTimestamps = null;
+                signatures = [],
+                signaturesTimestamps = [];
 
             //find optional parameters
             if (arguments.length >= 3) {
@@ -959,7 +959,7 @@ var qz = (function() {
             var signIdx = 0;
             for(var i = 0; i < configs.length || i < data.length; i++) {
                 var cfg = configs[Math.min(i, configs.length - 1)];
-                var ref = cfg.printer.name;
+                var ref = cfg.printer ? cfg.printer.name : null;
 
                 if (!map[ref]) {
                     map[ref] = {

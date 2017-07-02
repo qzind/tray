f7a0831a591e0cb34e1e539a0c28616893eed51f
Tres Finocchiaro <tres.finocchiaro@gmail.com>
2017-07-02 12:39:14 -0400

    Update python signing example Better exception handling, formatting

--- assets/signing/sign-message.py
+++ assets/signing/sign-message.py
@@ -22,22 +22,24 @@
 #                                                       #
 #########################################################
 
-
-import base64, os, sys
+import base64
+import os
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes, serialization
 from cryptography.hazmat.primitives.asymmetric import padding
-from django.shortcuts import HttpResponse
+from django.http import HttpResponse, HttpResponseBadRequest
 
 def index(request):
-    if (request.method == "GET"):
+    if request.method == 'GET':
         message = request.GET.get('request')
     else:
         message = request.POST.get('request')
 
+    if message == '' or message == None:
+        return HttpResponseBadRequest("Signing request needs 'request' parameter")
+
     mykey = os.path.join(os.path.dirname(__file__), "private-key.pem")
-    mypass = 'S3cur3P@ssw0rd'
-    # mypass = None
+    mypass = 'S3cur3P@ssw0rd' # or mypass = None
 
     # Load the private key
     key = serialization.load_pem_private_key(

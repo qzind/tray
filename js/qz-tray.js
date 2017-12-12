74749044e52661ac409822d15b1f53bc51993e91
bberenz <thebrettberenz@gmail.com>
2017-12-12 11:51:53 -0500

    onClose method still gets called, so we don't need to nullify connection here

--- js/qz-tray.js
+++ js/qz-tray.js
@@ -677,7 +677,6 @@ var qz = (function() {
                             if (options && count < options.retries) {
                                 attempt(count + 1);
                             } else {
-                                _qz.websocket.connection = null;
                                 reject.apply(null, arguments);
                             }
                         };

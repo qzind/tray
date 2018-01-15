c4dfbe1871bf312f09a77eeb178ca549a22f560a
bberenz <thebrettberenz@gmail.com>
2018-01-15 17:41:29 -0500

    Allow multiple direct prints

--- src/qz/printer/action/PrintDirect.java
+++ src/qz/printer/action/PrintDirect.java
@@ -11,20 +11,27 @@ import qz.printer.PrintOptions;
 import qz.printer.PrintOutput;
 import qz.utils.PrintingUtilities;
 
-import javax.print.*;
+import javax.print.DocFlavor;
+import javax.print.DocPrintJob;
+import javax.print.PrintException;
+import javax.print.SimpleDoc;
 import javax.print.attribute.HashPrintRequestAttributeSet;
 import javax.print.attribute.PrintRequestAttributeSet;
 import javax.print.attribute.standard.JobName;
-import java.io.*;
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Locale;
 
 public class PrintDirect extends PrintRaw {
 
     private static final Logger log = LoggerFactory.getLogger(PrintDirect.class);
 
-    private PrintingUtilities.Format format;
-    private String command;
+    private ArrayList<String> prints = new ArrayList<>();
+    private ArrayList<PrintingUtilities.Format> formats = new ArrayList<>();
 
 
     @Override
@@ -34,57 +41,57 @@ public class PrintDirect extends PrintRaw {
 
     @Override
     public void parseData(JSONArray printData, PrintOptions options) throws JSONException, UnsupportedOperationException {
-        if (printData.length() > 1) {
-            log.warn("Direct print only supports single data entries for printing");
+        for(int i = 0; i < printData.length(); i++) {
+            JSONObject data = printData.optJSONObject(i);
+            if (data == null) { continue; }
+
+            prints.add(data.getString("data"));
+            formats.add(PrintingUtilities.Format.valueOf(data.optString("format", "PLAIN").toUpperCase(Locale.ENGLISH)));
         }
-
-        JSONObject data = printData.optJSONObject(0);
-        if (data == null) { return; }
-
-        format = PrintingUtilities.Format.valueOf(data.optString("format", "PLAIN").toUpperCase(Locale.ENGLISH));
-        command = data.getString("data");
     }
 
     @Override
     public void print(PrintOutput output, PrintOptions options) throws PrintException {
-        DocPrintJob printJob = output.getPrintService().createPrintJob();
-
         PrintRequestAttributeSet attributes = new HashPrintRequestAttributeSet();
         attributes.add(new JobName(options.getRawOptions().getJobName(Constants.RAW_PRINT), Locale.getDefault()));
 
-        InputStream stream = null;
+        for(int i = 0; i < prints.size(); i++) {
+            DocPrintJob printJob = output.getPrintService().createPrintJob();
+            InputStream stream = null;
 
-        try {
-            switch(format) {
-                case BASE64:
-                    stream = new Base64InputStream(new ByteArrayInputStream(command.getBytes("UTF-8")));
-                    break;
-                case FILE:
-                    stream = new DataInputStream(new URL(command).openStream());
-                    break;
-                case PLAIN:
-                default:
-                    stream = new ByteArrayInputStream(command.getBytes("UTF-8"));
-                    break;
+            try {
+                switch(formats.get(i)) {
+                    case BASE64:
+                        stream = new Base64InputStream(new ByteArrayInputStream(prints.get(i).getBytes("UTF-8")));
+                        break;
+                    case FILE:
+                        stream = new DataInputStream(new URL(prints.get(i)).openStream());
+                        break;
+                    case PLAIN:
+                    default:
+                        stream = new ByteArrayInputStream(prints.get(i).getBytes("UTF-8"));
+                        break;
+                }
+
+                SimpleDoc doc = new SimpleDoc(stream, DocFlavor.INPUT_STREAM.AUTOSENSE, null);
+
+                waitForPrint(printJob, doc, attributes);
             }
-
-            SimpleDoc doc = new SimpleDoc(stream, DocFlavor.INPUT_STREAM.AUTOSENSE, null);
-
-            waitForPrint(printJob, doc, attributes);
-        }
-        catch(IOException e) {
-            throw new PrintException(e);
-        }
-        finally {
-            if (stream != null) {
-                try { stream.close(); } catch(Exception ignore) {}
+            catch(IOException e) {
+                throw new PrintException(e);
+            }
+            finally {
+                if (stream != null) {
+                    try { stream.close(); } catch(Exception ignore) {}
+                }
             }
         }
     }
 
     @Override
     public void cleanup() {
-        command = null;
+        prints.clear();
+        formats.clear();
     }
 
 }

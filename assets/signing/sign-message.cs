c40a830dc10c6582853dcc9e8c44e38f7d33c97e
Kyle LaBarge <kyle@Kyles-MacBook-Pro.local>
2018-05-17 20:33:59 -0400

    Update C# signing example

--- assets/signing/sign-message.cs
+++ assets/signing/sign-message.cs
@@ -1,7 +1,8 @@
 /**
  * Echoes the signed message and exits
  */
-public void SignMessage(String message)
+[HttpGet]
+public HttpResponseMessage SignMessage(String message)
 {
      // #########################################################
      // #             WARNING   WARNING   WARNING               #
@@ -25,17 +26,15 @@ public void SignMessage(String message)
     // Sample key.  Replace with one used for CSR generation
     // How to associate a private key with the X509Certificate2 class in .net
     // openssl pkcs12 -export -inkey private-key.pem -in digital-certificate.txt -out private-key.pfx
-	var KEY = "private-key.pfx";
-	//var PASS = "S3cur3P@ssw0rd";
+	var KEY = HttpContext.Current.Server.MapPath("~/private-key.pfx");
+	var PASS = "";
 
-	var cert = new X509Certificate2( KEY,/*PASS,*/ X509KeyStorageFlags.MachineKeySet | X509KeyStorageFlags.PersistKeySet | X509KeyStorageFlags.Exportable );
+	var cert = new X509Certificate2( KEY, PASS, X509KeyStorageFlags.MachineKeySet | X509KeyStorageFlags.PersistKeySet | X509KeyStorageFlags.Exportable );
 	RSACryptoServiceProvider csp = (RSACryptoServiceProvider)cert.PrivateKey;
 
 	byte[] data = new ASCIIEncoding().GetBytes(message);
 
 	byte[] hash = new SHA1Managed().ComputeHash(data);
 
-	Response.ContentType = "text/plain";
-	Response.Write(Convert.ToBase64String(csp.SignHash(hash, CryptoConfig.MapNameToOID("SHA1"))));
-	Environment.Exit(0)
+	var string response = Convert.ToBase64String(csp.SignHash(hash, CryptoConfig.MapNameToOID("SHA1")));
 }

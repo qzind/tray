4df9ca21d4c4aacf7c50112a549918dc83ef2598
tresf <tres.finocchiaro@gmail.com>
2018-10-19 22:57:48 -0400

    Initial Firefox policy support

---
+++ ant/firefox/json_writer.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import json
+import errno
+
+DEFAULT_PATH = '/Applications/Firefox.app/Contents/Resources/distribution/policies.json'
+DEFAULT_DATA = '{ "policies": { "Certificates": { "ImportEnterpriseRoots": true } } }'
+DEFAULT_OVERWRITE = False
+
+path = sys.argv[1] if len(sys.argv) > 1 else DEFAULT_PATH
+merge = json.loads(sys.argv[2]) if len(sys.argv) > 2 else DEFAULT_DATA
+overwrite = sys.argv[3].lower() == 'true' if len(sys.argv) > 3 else DEFAULT_OVERWRITE
+
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as e:  # Python >2.5
+        if e.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
+
+def load_json(path):
+    data = json.loads("{}")
+    if os.path.isfile(path):
+        try:
+            stream = open(path, "r")
+            data = json.load(stream)
+            stream.close()
+        except ValueError as e:
+            print("Warning: Not a valid JSON file: " + path)
+
+    return data
+
+policy = load_json(path)
+
+# Add only missing keys
+#{
+#  "policies": {
+#    "Certificates": {
+#      "ImportEnterpriseRoots": true
+#    }
+#  }
+#}
+
+# TODO: loop over merge object instead
+if policy.get('policies') == None:
+    policy['policies'] = {}
+
+if policy['policies'].get('Certificates') == None:
+    policy['policies']['Certificates'] = {}
+
+if policy['policies']['Certificates'].get('ImportEnterpriseRoots') == None:
+    policy['policies']['Certificates']['ImportEnterpriseRoots'] = True
+
+mkdir_p(os.path.dirname(path))
+stream = open(path, "w+")
+stream.write(json.dumps(policy, sort_keys=True, indent=2))
+stream.close()

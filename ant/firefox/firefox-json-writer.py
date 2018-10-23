22fb7222b0f83ab3a6078081d2896c90dccc3d7d
Berenz <thebrettberenz@gmail.com>
2018-10-23 00:06:39 -0400

    Add merge method for policy values

--- ant/firefox/firefox-json-writer.py
+++ ant/firefox/firefox-json-writer.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python
 
+import errno
+import json
 import os
 import sys
-import json
-import errno
 
 DEFAULT_PATH = '/Applications/Firefox.app/Contents/Resources/distribution/policies.json'
-# DEFAULT_DATA = '{ "policies": { "Certificates": { "ImportEnterpriseRoots": true } } }'
-# DEFAULT_OVERWRITE = False
+DEFAULT_DATA = '{ "policies": { "Certificates": { "ImportEnterpriseRoots": true } } }'
+DEFAULT_OVERWRITE = False
 
 path = sys.argv[1] if len(sys.argv) > 1 else DEFAULT_PATH
-# merge = json.loads(sys.argv[2]) if len(sys.argv) > 2 else json.loads(DEFAULT_DATA)
-# overwrite = sys.argv[3].lower() == 'true' if len(sys.argv) > 3 else DEFAULT_OVERWRITE
+merge = json.loads(sys.argv[2]) if len(sys.argv) > 2 else json.loads(DEFAULT_DATA)
+overwrite = sys.argv[3].lower() == 'true' if len(sys.argv) > 3 else DEFAULT_OVERWRITE
+
 
 def mkdir_p(path):
     try:
@@ -21,6 +22,7 @@ def mkdir_p(path):
             pass
         else:
             raise
+
 
 def load_json(path):
     data = json.loads("{}")
@@ -34,28 +36,21 @@ def load_json(path):
 
     return data
 
+
+def merge_json(base, append):
+    """ Writes values from append to base, deep copying if necessary """
+    for key, val in append.items():
+        if base.get(key) is None:
+            base[key] = val
+        elif type(base.get(key)) is dict and type(val) is dict:
+            merge_json(base.get(key), val)
+        elif overwrite:
+            # only forces overwrite if no deeper objects exist first
+            base[key] = val
+
+
 policy = load_json(path)
-
-# Add only missing keys
-#{
-#  "policies": {
-#    "Certificates": {
-#      "ImportEnterpriseRoots": true
-#    }
-#  }
-#}
-
-# FIXME: Replace this with a deep merge function
-# print(json.dumps(merge, sort_keys=True, indent=2))
-
-if policy.get('policies') == None:
-    policy['policies'] = {}
-
-if policy['policies'].get('Certificates') == None:
-    policy['policies']['Certificates'] = {}
-
-if policy['policies']['Certificates'].get('ImportEnterpriseRoots') == None:
-    policy['policies']['Certificates']['ImportEnterpriseRoots'] = True
+merge_json(policy, merge)
 
 mkdir_p(os.path.dirname(path))
 stream = open(path, "w+")

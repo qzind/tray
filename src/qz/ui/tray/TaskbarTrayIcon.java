1513d621a3fc5f5852f6e5ae7b0df0fea9243c26
Tres Finocchiaro <tres.finocchiaro@gmail.com>
2019-02-06 01:29:28 -0500

    Add taskbar menu for trayless OS (#411)
    - pseudo-tray behavior for task bar
    - better Fedora support

---
+++ src/qz/ui/tray/TaskbarTrayIcon.java
@@ -0,0 +1,134 @@
+package qz.ui.tray;
+
+import qz.common.Constants;
+import qz.utils.SystemUtilities;
+import qz.utils.UbuntuUtilities;
+
+import javax.swing.*;
+import javax.swing.event.PopupMenuEvent;
+import javax.swing.event.PopupMenuListener;
+import java.awt.*;
+import java.awt.event.*;
+import java.lang.reflect.Field;
+
+public class TaskbarTrayIcon extends JFrame implements WindowListener {
+    private Dimension iconSize;
+    private JPopupMenu popup;
+
+    public TaskbarTrayIcon(Image trayImage, final ActionListener exitListener) {
+        super(Constants.ABOUT_TITLE);
+        initializeComponents(trayImage, exitListener);
+    }
+
+    private void initializeComponents(Image trayImage, final ActionListener exitListener) {
+        // must come first
+        setUndecorated(true);
+        setTaskBarTitle(getTitle());
+        setSize(0,0);
+        getContentPane().setBackground(Color.BLACK);
+        if (SystemUtilities.isUbuntu()) {
+            // attempt to camouflage the single pixel left behind
+            getContentPane().setBackground(UbuntuUtilities.getTrayColor());
+        }
+
+        iconSize = new Dimension(40, 40);
+
+        setIconImage(trayImage);
+        setResizable(false);
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                exitListener.actionPerformed(new ActionEvent(e.getComponent(), e.getID(), "Exit"));
+            }
+        });
+        addWindowListener(this);
+    }
+
+    // fixes Linux taskbar title per http://hg.netbeans.org/core-main/rev/5832261b8434
+    public static void setTaskBarTitle(String title) {
+        try {
+            Class<?> toolkit = Toolkit.getDefaultToolkit().getClass();
+            if (toolkit.getName().equals("sun.awt.X11.XToolkit")) {
+                final Field awtAppClassName = toolkit.getDeclaredField("awtAppClassName");
+                awtAppClassName.setAccessible(true);
+                awtAppClassName.set(null, title);
+            }
+        } catch(Exception ignore) {}
+    }
+
+    /**
+     * Returns the "tray" icon size (not the dialog size)
+     */
+    @Override
+    public Dimension getSize() {
+        return iconSize;
+    }
+
+    public void setJPopupMenu(final JPopupMenu popup) {
+        this.popup = popup;
+        this.popup.addPopupMenuListener(new PopupMenuListener() {
+            @Override
+            public void popupMenuWillBecomeVisible(PopupMenuEvent popupMenuEvent) {}
+
+            @Override
+            public void popupMenuWillBecomeInvisible(PopupMenuEvent popupMenuEvent) {
+                setState(JFrame.ICONIFIED);
+            }
+
+            @Override
+            public void popupMenuCanceled(PopupMenuEvent popupMenuEvent) {
+                setState(JFrame.ICONIFIED);
+            }
+        });
+        this.popup.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent keyEvent) {}
+
+            @Override
+            public void keyPressed(KeyEvent keyEvent) {
+                if (keyEvent.getKeyCode() == KeyEvent.VK_ESCAPE) {
+                    TaskbarTrayIcon.this.popup.setVisible(false);
+                }
+            }
+
+            @Override
+            public void keyReleased(KeyEvent keyEvent) {}
+        });
+    }
+
+    public void displayMessage(String caption, String text, TrayIcon.MessageType level) { /* noop */ }
+
+    @Override
+    public void windowDeiconified(WindowEvent e) {
+        Point p = MouseInfo.getPointerInfo().getLocation();
+        setLocation(p);
+        // call show against parent to prevent un-clickable state
+        popup.show(this, 0,0);
+
+        // move to mouse cursor; adjusting for screen boundaries
+        Point before = popup.getLocationOnScreen();
+        Point after = new Point();
+        after.setLocation(before.x < p.x ? p.x - popup.getWidth() : p.x, before.y < p.y ? p.y - popup.getHeight() : p.y);
+        popup.setLocation(after);
+    }
+
+    @Override
+    public void windowOpened(WindowEvent windowEvent) {}
+
+    @Override
+    public void windowClosing(WindowEvent windowEvent) {}
+
+    @Override
+    public void windowClosed(WindowEvent windowEvent) {}
+
+    @Override
+    public void windowIconified(WindowEvent windowEvent) {}
+
+    @Override
+    public void windowActivated(WindowEvent windowEvent) {}
+
+    @Override
+    public void windowDeactivated(WindowEvent windowEvent) {
+        popup.setVisible(false);
+        setState(JFrame.ICONIFIED);
+    }
+}
---
+++ src/qz/ui/tray/TrayType.java
@@ -0,0 +1,90 @@
+package qz.ui.tray;
+
+import org.jdesktop.swinghelper.tray.JXTrayIcon;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+
+/**
+ * Wrapper class to allow popup menu on a tray-less OS
+ * @author Tres Finocchiaro
+ */
+public enum TrayType {
+    JX,
+    CLASSIC,
+    MODERN,
+    TASKBAR;
+
+    private JXTrayIcon tray = null;
+    private TaskbarTrayIcon taskbar = null;
+
+    public JXTrayIcon tray() { return tray; }
+
+    public TrayType init() {
+        return init(null);
+    }
+
+    public TrayType init(ActionListener exitListener) {
+        switch (this) {
+            case JX:
+                tray = new JXTrayIcon(blankImage());
+            case CLASSIC:
+                tray = new ClassicTrayIcon(blankImage());
+            case MODERN:
+                tray = new ModernTrayIcon(blankImage());
+            default:
+                taskbar = new TaskbarTrayIcon(blankImage(), exitListener);
+        }
+        return this;
+    }
+
+    private static Image blankImage() {
+        return new ImageIcon(new byte[1]).getImage();
+    }
+
+    public boolean isTray() { return tray != null; }
+
+    public boolean getTaskbar() { return taskbar != null; }
+
+    public void setImage(Image image) {
+        if (isTray()) {
+            tray.setImage(image);
+        } else {
+            taskbar.setIconImage(image);
+        }
+    }
+
+    public Dimension getSize() {
+        return isTray() ? tray.getSize() : taskbar.getSize();
+    }
+
+    public void setToolTip(String tooltip) {
+        if (isTray()) {
+            tray.setToolTip(tooltip);
+        }
+    }
+
+    public void setJPopupMenu(JPopupMenu popup) {
+        if (isTray()) {
+            tray.setJPopupMenu(popup);
+        } else {
+            taskbar.setJPopupMenu(popup);
+        }
+    }
+
+    public void displayMessage(String caption, String text, TrayIcon.MessageType level) {
+        if (isTray()) {
+            tray.displayMessage(caption, text, level);
+        } else {
+            taskbar.displayMessage(caption, text, level);
+        }
+    }
+
+    public void showTaskbar() {
+        if (getTaskbar()) {
+            taskbar.setVisible(true);
+            taskbar.setState(Frame.ICONIFIED);
+        }
+    }
+}

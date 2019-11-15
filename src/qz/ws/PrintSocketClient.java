344458603f04ef3337dbdd0ccc7f60f2d0b7a087
Tres Finocchiaro <tres.finocchiaro@gmail.com>
2019-11-14 20:58:03 -0500

    Update PrintSocketClient.java

--- src/qz/ws/PrintSocketClient.java
+++ src/qz/ws/PrintSocketClient.java
@@ -6,7 +6,6 @@ import org.apache.commons.io.IOUtils;
 import org.codehaus.jettison.json.JSONArray;
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
-import org.eclipse.jetty.io.EofException;
 import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.api.WebSocketException;
 import org.eclipse.jetty.websocket.api.annotations.*;
@@ -162,7 +161,6 @@ public class PrintSocketClient {
     @OnWebSocketError
     public void onError(Session session, Throwable error) {
         if (error instanceof EOFException) return;
-        if (error instanceof EofException) return;
         log.error("Connection error", error);
         trayManager.displayErrorMessage(error.getMessage());
     }

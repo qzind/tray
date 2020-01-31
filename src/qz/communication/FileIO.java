9454377b75f0d7b3c51d28699c2929964414ed70
Kyle Berezin <Kyle@Berezin.com>
2020-01-30 22:49:06 -0500

    FileIO Filtering (#562)
    Add FileIO filtering via { include: ..., exclude: ... } params

--- src/qz/communication/FileIO.java
+++ src/qz/communication/FileIO.java
@@ -1,5 +1,8 @@
 package qz.communication;
 
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.IOCase;
+import org.codehaus.jettison.json.JSONArray;
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 import org.eclipse.jetty.websocket.api.Session;
@@ -8,6 +11,7 @@ import qz.ws.StreamEvent;
 
 import java.nio.file.Path;
 import java.nio.file.WatchKey;
+import java.util.ArrayList;
 
 public class FileIO {
     public static final String SANDBOX_DATA_SUFFIX = "sandbox";
@@ -30,10 +34,33 @@ public class FileIO {
     private long bytes;
     private int lines;
 
+    private IOCase caseSensitivity;
+
+    private ArrayList<String> inclusions;
+    private ArrayList<String> exclusions;
+
     public FileIO(Session session, JSONObject params, Path originalPath, Path absolutePath) throws JSONException {
         this.session = session;
         this.originalPath = originalPath;
         this.absolutePath = absolutePath;
+
+        inclusions = new ArrayList<>();
+        exclusions = new ArrayList<>();
+
+        JSONArray inc = params.optJSONArray("include");
+        JSONArray exc = params.optJSONArray("exclude");
+        caseSensitivity = params.optBoolean("ignoreCase", true) ? IOCase.INSENSITIVE : IOCase.SENSITIVE;
+
+        if (inc != null) {
+            for (int i = 0; i < inc.length(); i++) {
+                inclusions.add(inc.getString(i));
+            }
+        }
+        if (exc != null) {
+            for(int i = 0; i < exc.length(); i++) {
+                exclusions.add(exc.getString(i));
+            }
+        }
 
         JSONObject options = params.optJSONObject("listener");
         if (options != null) {
@@ -48,6 +75,26 @@ public class FileIO {
             lines = options.optInt("lines", readType == ReadType.LINES? FILE_LISTENER_DEFAULT_LINES:-1);
             reversed = options.optBoolean("reverse", readType == ReadType.LINES);
         }
+    }
+
+    private boolean isMatch(String fileName) {
+        boolean match = inclusions.isEmpty();
+        for (String inclusion : inclusions) {
+            if(FilenameUtils.wildcardMatch(fileName, inclusion, caseSensitivity)) {
+                match = true;
+                break;
+            }
+        }
+
+        if(match) {
+            for(String exclusion : exclusions) {
+                if (FilenameUtils.wildcardMatch(fileName, exclusion, caseSensitivity)) {
+                    match = false;
+                    break;
+                }
+            }
+        }
+        return match;
     }
 
     public boolean returnsContents() {
@@ -87,6 +134,8 @@ public class FileIO {
     }
 
     public void fileChanged(String fileName, String type, String fileData) {
+        if (!isMatch(fileName)) return;
+
         StreamEvent evt = new StreamEvent(StreamEvent.Stream.FILE, StreamEvent.Type.ACTION)
                 .withData("file", getOriginalPath().resolve(fileName))
                 .withData("eventType", type);

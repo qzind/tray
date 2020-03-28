37996e2e0c89f6550440bb5934b3846c44deb50a
Berenz <thebrettberenz@gmail.com>
2020-03-28 17:40:37 -0400

    Bring forward 2.0 html raster refactoring + fix existing zoom/scaling issues

--- src/qz/printer/action/WebApp.java
+++ src/qz/printer/action/WebApp.java
@@ -1,26 +1,21 @@
 package qz.printer.action;
 
 import com.github.zafarkhaja.semver.Version;
-import javafx.animation.PauseTransition;
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
 import javafx.concurrent.Worker;
 import javafx.embed.swing.SwingFXUtils;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.print.PageLayout;
 import javafx.print.PrinterJob;
 import javafx.scene.Scene;
-import javafx.scene.SnapshotParameters;
-import javafx.scene.image.WritableImage;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.transform.Scale;
 import javafx.scene.transform.Transform;
 import javafx.scene.transform.Translate;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
-import javafx.util.Duration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.w3c.dom.Attr;
@@ -34,7 +29,8 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Method;
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
 
 /**
@@ -47,22 +43,19 @@ public class WebApp extends Application {
 
     private static final Logger log = LoggerFactory.getLogger(WebApp.class);
 
-    private static final int SLEEP = 250;
-    private static final int TIMEOUT = 60; //total paused seconds before failing
-
-    private static final double WEB_SCALE = 72d / 96d;
-
     private static WebApp instance = null;
 
     private static Stage stage;
     private static WebView webView;
     private static double pageHeight;
+    private static double pageZoom;
 
-    private static final AtomicBoolean startup = new AtomicBoolean(false);
-    private static final AtomicBoolean complete = new AtomicBoolean(false);
+    private static CountDownLatch startupLatch;
+    private static CountDownLatch captureLatch;
+
+    private static Runnable printAction;
     private static final AtomicReference<Throwable> thrown = new AtomicReference<>();
 
-    private static PauseTransition snap;
 
     //listens for a Succeeded state to activate image capture
     private static ChangeListener<Worker.State> stateListener = (ov, oldState, newState) -> {
@@ -85,7 +78,7 @@ public class WebApp extends Application {
             //width was resized earlier (for responsive html), then calculate the best fit height
             if (pageHeight <= 0) {
                 String heightText = webView.getEngine().executeScript("Math.max(document.body.offsetHeight, document.body.scrollHeight)").toString();
-                pageHeight = Double.parseDouble(heightText);
+                pageHeight = Double.parseDouble(heightText) * pageZoom;
 
                 log.trace("Setting HTML page height to {}", pageHeight);
                 webView.setMinHeight(pageHeight);
@@ -94,10 +87,7 @@ public class WebApp extends Application {
                 autosize(webView);
             }
 
-            //scale web dimensions down to print dpi
-            webView.getTransforms().add(new Scale(WEB_SCALE, WEB_SCALE));
-
-            snap.playFromStart();
+            printAction.run();
         }
     };
 
@@ -106,7 +96,7 @@ public class WebApp extends Application {
 
     //listens for failures
     private static ChangeListener<Throwable> exceptListener = (obs, oldExc, newExc) -> {
-        if (newExc != null) { thrown.set(newExc); }
+        if (newExc != null) { unlatch(newExc); }
     };
 
 
@@ -123,30 +113,31 @@ public class WebApp extends Application {
         }
 
         if (instance == null) {
+            startupLatch = new CountDownLatch(1);
             new Thread(() -> Application.launch(WebApp.class)).start();
-            startup.set(false);
         }
 
-        for(int i = 0; i < (TIMEOUT * 1000); i += SLEEP) {
-            if (startup.get()) { break; }
-
-            log.trace("Waiting for JavaFX..");
-            try { Thread.sleep(SLEEP); } catch(Exception ignore) {}
-        }
-
-        if (!startup.get()) {
-            throw new IOException("JavaFX did not start");
+        if (startupLatch.getCount() > 0) {
+            try {
+                log.trace("Waiting for JavaFX..");
+                if (!startupLatch.await(60, TimeUnit.SECONDS)) {
+                    throw new IOException("JavaFX did not start");
+                }
+            }
+            catch(InterruptedException ignore) {}
         }
     }
 
     @Override
     public void start(Stage st) throws Exception {
-        startup.set(true);
+        startupLatch.countDown();
         log.debug("Started JavaFX");
 
         webView = new WebView();
         st.setScene(new Scene(webView));
         stage = st;
+        stage.setWidth(1);
+        stage.setHeight(1);
 
         Worker<Void> worker = webView.getEngine().getLoadWorker();
         worker.stateProperty().addListener(stateListener);
@@ -165,15 +156,17 @@ public class WebApp extends Application {
      * @throws Throwable JavaFx will throw a generic {@code Throwable} class for any issues
      */
     public static synchronized void print(final PrinterJob job, final WebAppModel model) throws Throwable {
-        load(model, (event) -> {
+        model.setZoom(1); //vector prints do not need to use zoom
+
+        load(model, () -> {
             try {
                 PageLayout layout = job.getJobSettings().getPageLayout();
                 if (model.isScaled()) {
                     double scale;
-                    if ((webView.getWidth() / webView.getHeight()) / WEB_SCALE >= (layout.getPrintableWidth() / layout.getPrintableHeight())) {
-                        scale = (layout.getPrintableWidth() / webView.getWidth()) / WEB_SCALE;
+                    if ((webView.getWidth() / webView.getHeight()) >= (layout.getPrintableWidth() / layout.getPrintableHeight())) {
+                        scale = (layout.getPrintableWidth() / webView.getWidth());
                     } else {
-                        scale = (layout.getPrintableHeight() / webView.getHeight()) / WEB_SCALE;
+                        scale = (layout.getPrintableHeight() / webView.getHeight());
                     }
                     webView.getTransforms().add(new Scale(scale, scale));
                 }
@@ -208,54 +201,61 @@ public class WebApp extends Application {
                     //reset state
                     webView.getTransforms().remove(activePage);
 
-                    complete.set(true);
+                    unlatch(null);
                 });
             }
-            catch(Exception e) { thrown.set(e); }
-            finally { stage.hide(); }
+            catch(Exception e) { unlatch(e); }
         });
 
-        Throwable t = null;
-        while((job.getJobStatus() == PrinterJob.JobStatus.NOT_STARTED || job.getJobStatus() == PrinterJob.JobStatus.PRINTING)
-                && !complete.get() && (t = thrown.get()) == null) {
-            log.trace("Waiting on print..");
-            try { Thread.sleep(1000); } catch(Exception ignore) {}
-        }
+        log.trace("Waiting on print..");
+        captureLatch.await(); //released when unlatch is called
 
-        if (t != null) { throw t; }
+        if (thrown.get() != null) { throw thrown.get(); }
     }
 
     public static synchronized BufferedImage raster(final WebAppModel model) throws Throwable {
         AtomicReference<BufferedImage> capture = new AtomicReference<>();
 
         //ensure JavaFX has started before we run
-        if (!startup.get()) {
+        if (startupLatch.getCount() > 0) {
             throw new IOException("JavaFX has not been started");
         }
 
+        //raster still needs to show stage for valid capture
         Platform.runLater(() -> {
             stage.show();
             stage.toBack();
         });
 
-        load(model, (event) -> {
-            try {
-                WritableImage snapshot = webView.snapshot(new SnapshotParameters(), null);
-                capture.set(SwingFXUtils.fromFXImage(snapshot, null));
+        //adjust raster prints to web dpi
+        double increase = 96d / 72d;
+        model.setWebWidth(model.getWebWidth() * increase);
+        model.setWebHeight(model.getWebHeight() * increase);
 
-                complete.set(true);
+        load(model, () -> Platform.runLater(() -> new AnimationTimer() {
+            int frames = 0;
+
+            @Override
+            public void handle(long l) {
+                if (++frames == 2) {
+                    log.debug("Attempting image capture");
+
+                    webView.snapshot((snapshotResult) -> {
+                        capture.set(SwingFXUtils.fromFXImage(snapshotResult.getImage(), null));
+                        unlatch(null);
+
+                        return null;
+                    }, null, null);
+
+                    stop();
+                }
             }
-            catch(Throwable t) { thrown.set(t); }
-            finally { stage.hide(); }
-        });
+        }.start()));
 
-        Throwable t = null;
-        while(!complete.get() && (t = thrown.get()) == null) {
-            log.trace("Waiting on capture..");
-            try { Thread.sleep(1000); } catch(Exception ignore) {}
-        }
+        log.trace("Waiting on capture..");
+        captureLatch.await(); //released when unlatch is called
 
-        if (t != null) { throw t; }
+        if (thrown.get() != null) { throw thrown.get(); }
 
         return capture.get();
     }
@@ -266,25 +266,34 @@ public class WebApp extends Application {
      * @param model  The model specifying the web page parameters.
      * @param action EventHandler that will be ran when the WebView completes loading.
      */
-    private static synchronized void load(WebAppModel model, EventHandler<ActionEvent> action) {
-        complete.set(false);
+    private static synchronized void load(WebAppModel model, Runnable action) {
+        captureLatch = new CountDownLatch(1);
         thrown.set(null);
 
         Platform.runLater(() -> {
-            log.trace("Setting starting size {}:{}", model.getWebWidth(), model.getWebHeight());
-            webView.setMinSize(model.getWebWidth(), model.getWebHeight());
-            webView.setPrefSize(model.getWebWidth(), model.getWebHeight());
-            webView.setMaxSize(model.getWebWidth(), model.getWebHeight());
-            autosize(webView);
+            //zoom should only be factored on raster prints
+            pageZoom = model.getZoom();
+            double pageWidth = model.getWebWidth() * pageZoom;
+            pageHeight = model.getWebHeight() * pageZoom;
 
-            pageHeight = model.getWebHeight();
+            log.trace("Setting starting size {}:{}", pageWidth, pageHeight);
+            webView.setMinSize(pageWidth, pageHeight);
+            webView.setPrefSize(pageWidth, pageHeight);
+            webView.setMaxSize(pageWidth, pageHeight);
+
+            if (pageHeight == 0) {
+                webView.setMinHeight(1);
+                webView.setPrefHeight(1);
+                webView.setMaxHeight(1);
+            }
 
             //reset additive properties
             webView.getTransforms().clear();
-            webView.setZoom(1.0);
+            webView.setZoom(model.getZoom());
 
-            snap = new PauseTransition(Duration.millis(100));
-            snap.setOnFinished(action);
+            autosize(webView);
+
+            printAction = action;
 
             if (model.isPlainText()) {
                 webView.getEngine().loadContent(model.getSource(), "text/html");
@@ -317,6 +326,18 @@ public class WebApp extends Application {
         } catch(SecurityException | ReflectiveOperationException e) {
             log.warn("Unable to update peer; Blank pages may occur.", e);
         }
+    }
+
+    /**
+     * Final cleanup when no longer capturing
+     */
+    public static void unlatch(Throwable t) {
+        if (t != null) {
+            thrown.set(t);
+        }
+
+        captureLatch.countDown();
+        stage.hide();
     }
 
 }
--- src/qz/printer/action/WebAppModel.java
+++ src/qz/printer/action/WebAppModel.java
@@ -3,21 +3,18 @@ package qz.printer.action;
 public class WebAppModel {
 
     private String source;
-    private boolean plainText = false;
+    private boolean plainText;
 
-    private double webWidth = 0.0;
-    private double webHeight = 0.0;
-    private boolean isScaled = true;
-    private double zoom = 1.0;
+    private double webWidth;
+    private double webHeight;
+    private boolean isScaled;
+    private double zoom;
 
     public WebAppModel(String source, boolean plainText, double webWidth, double webHeight, boolean isScaled, double zoom) {
-        //values supplied are at print dpi, scale up to web dpi here
-        double increase = 96d / 72d;
-
         this.source = source;
         this.plainText = plainText;
-        this.webWidth = webWidth * increase;
-        this.webHeight = webHeight * increase;
+        this.webWidth = webWidth;
+        this.webHeight = webHeight;
         this.isScaled = isScaled;
         this.zoom = zoom;
     }
@@ -26,20 +23,40 @@ public class WebAppModel {
         return source;
     }
 
+    public void setSource(String source) {
+        this.source = source;
+    }
+
     public boolean isPlainText() {
         return plainText;
+    }
+
+    public void setPlainText(boolean plainText) {
+        this.plainText = plainText;
     }
 
     public double getWebWidth() {
         return webWidth;
     }
 
+    public void setWebWidth(double webWidth) {
+        this.webWidth = webWidth;
+    }
+
     public double getWebHeight() {
         return webHeight;
     }
 
+    public void setWebHeight(double webHeight) {
+        this.webHeight = webHeight;
+    }
+
     public boolean isScaled() {
         return isScaled;
+    }
+
+    public void setScaled(boolean scaled) {
+        isScaled = scaled;
     }
 
     public double getZoom() {
@@ -49,5 +66,4 @@ public class WebAppModel {
     public void setZoom(double zoom) {
         this.zoom = zoom;
     }
-
 }

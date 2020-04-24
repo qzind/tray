498040dd6ac344452e44840058a64538822e0979
tresf <tres.finocchiaro@gmail.com>
2020-04-24 00:06:16 -0400

    Finish removing Sha256 Revert subtle.crypto since we bundle our own hasher

--- js/qz-tray.js
+++ js/qz-tray.js
@@ -574,26 +574,6 @@ var qz = (function() {
             hash: function(data) {
                 if(typeof Sha256 !== 'undefined') {
                     return Sha256.hash(data);
-                } else if(typeof window !== 'undefined' && window.isSecureContext && typeof TextEncoder === 'object') {
-                    // Use native crypto (HTTPS only)
-                    var buffer = new TextEncoder('utf-8').encode(str)
-                    return crypto.subtle.digest('SHA-256', buffer).then(function (hash) {
-                        var hexCodes = []
-                        var view = new DataView(buffer)
-                        for (var i = 0; i < view.byteLength; i += 4) {
-                            // Using getUint32 reduces the number of iterations needed (we process 4 bytes each time)
-                            var value = view.getUint32(i)
-                            // toString(16) will give the hex representation of the number without padding
-                            var stringValue = value.toString(16)
-                            // We use concatenation and slice for padding
-                            var padding = '00000000'
-                            var paddedValue = (padding + stringValue).slice(-padding.length)
-                            hexCodes.push(paddedValue)
-                        }
-
-                        // Join all the hex strings into one
-                        return hexCodes.join('')
-                    });
                 } else {
                     return _qz.SHA.hash(data);
                 }

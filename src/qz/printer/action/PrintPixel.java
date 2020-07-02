f7c33f6037d969b45a82b18c7f072ca35ca6fbb4
Berenz <thebrettberenz@gmail.com>
2020-07-01 23:49:30 -0400

    Tray list checks by instance Option matching tries to be less exact

--- src/qz/printer/action/PrintPixel.java
+++ src/qz/printer/action/PrintPixel.java
@@ -23,14 +23,14 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public abstract class PrintPixel {
 
     private static final Logger log = LoggerFactory.getLogger(PrintPixel.class);
 
     private static final List<Integer> MAC_BAD_IMAGE_TYPES = Arrays.asList(BufferedImage.TYPE_BYTE_BINARY, BufferedImage.TYPE_CUSTOM);
-
-    private static final List<String> PRINTER_TRAY_ALIASES = Arrays.asList("", "Tray ", "Paper Cassette ");
 
 
     protected PrintRequestAttributeSet applyDefaultSettings(PrintOptions.Pixel pxlOpts, PageFormat page, Media[] supported) {
@@ -45,13 +45,34 @@ public abstract class PrintPixel {
             attributes.add(pxlOpts.getOrientation().getAsOrientRequested());
         }
         if (pxlOpts.getPrinterTray() != null && !pxlOpts.getPrinterTray().isEmpty()) {
+            Pattern exactPattern = Pattern.compile("\\b" + Pattern.quote(pxlOpts.getPrinterTray()) + "\\b", Pattern.CASE_INSENSITIVE);
+            Pattern fuzzyPattern = Pattern.compile("\\b.*?[" + Pattern.quote(pxlOpts.getPrinterTray()) +"]+.*?\\b", Pattern.CASE_INSENSITIVE);
+            Media bestFit = null;
+            Integer fuzzyFitDelta = null;
+
             for(Media m : supported) {
-                for(String pta : PRINTER_TRAY_ALIASES) {
-                    if (m.toString().trim().equalsIgnoreCase(pta + pxlOpts.getPrinterTray().trim())) {
-                        attributes.add(m);
+                if (m instanceof MediaTray) {
+                    Matcher exactly = exactPattern.matcher(m.toString().trim());
+                    Matcher fuzzily = fuzzyPattern.matcher(m.toString().trim());
+
+                    if (exactly.find()) {
+                        bestFit = m;
                         break;
                     }
+
+                    while(fuzzily.find()) {
+                        //look for as close to exact match as possible
+                        int delta = Math.abs(fuzzily.group().length() - pxlOpts.getPrinterTray().length());
+                        if (fuzzyFitDelta == null || delta < fuzzyFitDelta) {
+                            fuzzyFitDelta = delta;
+                            bestFit = m;
+                        }
+                    }
                 }
+            }
+
+            if (bestFit != null) {
+                attributes.add(bestFit);
             }
         }
 

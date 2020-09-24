3b77e06817a0ff39f4d648a14cb091e5961e0bd7
Berenz <thebrettberenz@gmail.com>
2020-09-24 19:21:44 -0400

    Don't provide an explicit dpi on vector pdf prints

--- src/qz/printer/action/PrintPDF.java
+++ src/qz/printer/action/PrintPDF.java
@@ -152,8 +152,8 @@ public class PrintPDF extends PrintPixel implements PrintProcessor {
 
         if (!pxlOpts.isRasterize()) {
             if (pxlOpts.getDensity() > 0) {
-                //rasterization is automatically performed upon supplying a density, warn user if they aren't expecting this
-                log.warn("Supplying a print density for PDF printing rasterizes the document.");
+                // clear density for vector prints (applied via print attributes instead)
+                useDensity = 0;
             } else if (SystemUtilities.isMac() && Constants.JAVA_VERSION.compareWithBuildsTo(Version.valueOf("1.8.0+121")) < 0) {
                 log.warn("OSX systems cannot print vector PDF's, forcing raster to prevent crash.");
                 useDensity = options.getDefaultOptions().getDensity();

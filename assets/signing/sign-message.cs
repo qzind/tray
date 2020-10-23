3829d7086afa3749e7d8cea79b6a461a5833b355
Tres Finocchiaro <tres.finocchiaro@gmail.com>
2020-10-23 14:51:51 -0400

    Update sign-message.cs
    Add MVC notes

--- assets/signing/sign-message.cs
+++ assets/signing/sign-message.cs
@@ -26,6 +26,19 @@ using System.Web.Services;
 // openssl pkcs12 -export -inkey private-key.pem -in digital-certificate.txt -out private-key.pfx
 private static X509KeyStorageFlags STORAGE_FLAGS = X509KeyStorageFlags.MachineKeySet | X509KeyStorageFlags.PersistKeySet | X509KeyStorageFlags.Exportable;
 
+/**
+ * Note, this example is for .NET Forms/PageMethods
+ * For MVC, change the following:
+ * 
+ * public ActionResult SignMessage() {
+ *      string request = Request.QueryString["request"];
+ *      ...
+ *      return Content(base64, "text/plain");
+ *      ...
+ *      return SignMessage();
+ * 
+ * ... and replace PageMethods calls with fetch("@Url.Content("./SignMessage/?request=")" + toSign
+ */
 [WebMethod]
 public static string SignMessage(string request)
 {
@@ -43,7 +56,8 @@ public static string SignMessage(string request)
         RSACryptoServiceProvider cspStrong = new RSACryptoServiceProvider(); // 2.1 and higher: Make RSACryptoServiceProvider that can handle SHA256, SHA512
         cspStrong.ImportParameters(csp.ExportParameters(true));	// Copy to stronger RSACryptoServiceProvider
         byte[] hash = new SHA512CryptoServiceProvider().ComputeHash(data);  // Use SHA1CryptoServiceProvider for QZ Tray 2.0 and older
-        return Convert.ToBase64String(cspStrong.SignHash(hash, CryptoConfig.MapNameToOID("SHA512"))); // Use "SHA1" for QZ Tray 2.0 and older
+        string base64 = Convert.ToBase64String(cspStrong.SignHash(hash, CryptoConfig.MapNameToOID("SHA512"))); // Use "SHA1" for QZ Tray 2.0 and older
+	return base64;
     }
     catch(Exception ex)
     {

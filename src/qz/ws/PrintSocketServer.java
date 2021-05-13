85c33b4fe54acfe65eca41b51b31d718cede4ca9
Tres Finocchiaro <tres.finocchiaro@gmail.com>
2021-05-13 16:28:21 -0400

    Restore license header

--- src/qz/ws/PrintSocketServer.java
+++ src/qz/ws/PrintSocketServer.java
@@ -1,5 +1,11 @@
 /**
+ * @author Robert Casto
  *
+ * Copyright (C) 2016 Tres Finocchiaro, QZ Industries, LLC
+ *
+ * LGPL 2.1 This is free software.  This software and source code are released under
+ * the "LGPL 2.1 License".  A copy of this license should be distributed with
+ * this software. http://www.gnu.org/licenses/lgpl-2.1.html
  */
 
 package qz.ws;

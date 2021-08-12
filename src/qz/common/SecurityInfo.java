580d2ef0d1cab0d463e9c5555368f8fa977eac72
Tres Finocchiaro <tres.finocchiaro@gmail.com>
2021-08-11 21:37:04 -0400

    Minor fixes

--- src/qz/common/SecurityInfo.java
+++ src/qz/common/SecurityInfo.java
@@ -100,9 +100,14 @@ public class SecurityInfo {
             libVersions.put("javafx (location)", "Failed");
         }
 
-        // load the jna class which initializes the needed properties
-        int ignore = Native.BOOL_SIZE;
-        libVersions.put("jna lib location", System.getProperty("jnidispatch.path"));
+        // Implicitly call Native.loadNativeDispatchLibrary() to populate "jnidispatch.path"
+        try {
+            @SuppressWarnings("unused")
+            int ignore = Native.BOOL_SIZE;
+            libVersions.put("jna (location)", System.getProperty("jnidispatch.path"));
+        } catch(Throwable t) {
+            libVersions.put("jna (location)", "Failed");
+        }
 
         // Fallback to maven manifest information
         HashMap<String,String> mavenVersions = getMavenVersions();

0e0ed8a4cbb6f15fbb9d75ac7e21e584bcf7c5fe
Tres Finocchiaro <tres.finocchiaro@gmail.com>
2021-08-13 22:22:58 -0400

    Various ant fixes

--- src/qz/build/JLink.java
+++ src/qz/build/JLink.java
@@ -27,7 +27,7 @@ public class JLink {
     private static final Logger log = LoggerFactory.getLogger(JLink.class);
     private static final String JAVA_AMD64_VENDOR = "AdoptOpenJDK";
     private static final String JAVA_ARM64_VENDOR = "BellSoft";
-    private static final String JAVA_VERSION = "11.0.11+10";;
+    private static final String JAVA_VERSION = "11.0.11+9";;
     private static final String JAVA_MAJOR = JAVA_VERSION.split("\\.")[0];
     private static final String JAVA_MINOR = JAVA_VERSION.split("\\.")[1];
     private static final String JAVA_PATCH = JAVA_VERSION.split("\\.|\\+|-")[2];

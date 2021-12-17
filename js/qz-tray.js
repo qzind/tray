7a1f634b0d93aa90643f13313f2def8937381fdd
tresf <tres.finocchiaro@gmail.com>
2021-12-16 20:55:02 -0500

    Bump version

--- js/qz-tray.js
+++ js/qz-tray.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /**
- * @version 2.2.0
+ * @version 2.2.1
  * @overview QZ Tray Connector
  * <p/>
  * Connects a web client to the QZ Tray software.
@@ -38,7 +38,7 @@ var qz = (function() {
 ///// PRIVATE METHODS /////
 
     var _qz = {
-        VERSION: "2.2.0",                              //must match @version above
+        VERSION: "2.2.1",                              //must match @version above
         DEBUG: false,
 
         log: {

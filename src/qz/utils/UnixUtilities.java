6bf6eb54e6d3cc0df2cab582260d8ea1a2add1b7
Tres Finocchiaro <tres.finocchiaro@gmail.com>
2022-11-07 23:45:52 -0500

    Fix typo

--- src/qz/utils/UnixUtilities.java
+++ src/qz/utils/UnixUtilities.java
@@ -35,7 +35,7 @@ public class UnixUtilities {
     private static final String[] OS_NAME_KEYS = {"NAME", "DISTRIB_ID"};
     private static final String[] OS_VERSION_KEYS = {"VERSION", "DISTRIB_RELEASE"};
     private static final String[] KNOWN_ELEVATORS = {"pkexec", "gksu", "gksudo", "kdesudo" };
-    private static final String[] OS_RELEASE_FILES = {"/etc/os-release", "/usr/lib/os-release", "/etc/lsbg-release", "/etc/redhat-release"};
+    private static final String[] OS_RELEASE_FILES = {"/etc/os-release", "/usr/lib/os-release", "/etc/lsb-release", "/etc/redhat-release"};
     private static String uname;
     private static String unixRelease;
     private static String unixVersion;

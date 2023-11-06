7ee9ae075c61c36282cc483c5cb9d5a44ef89405
Tres Finocchiaro <tres.finocchiaro@gmail.com>
2023-11-06 10:11:50 -0500

    Add support for 2400 baud Closes #1211 Closes #476

--- src/qz/utils/SerialUtilities.java
+++ src/qz/utils/SerialUtilities.java
@@ -19,6 +19,8 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 
+import static jssc.SerialPort.*;
+
 /**
  * @author Tres
  */
@@ -26,13 +28,20 @@ public class SerialUtilities {
 
     private static final Logger log = LogManager.getLogger(SerialUtilities.class);
 
-    private static final List<Integer> VALID_BAUD = Arrays.asList(SerialPort.BAUDRATE_110, SerialPort.BAUDRATE_300,
-                                                                  SerialPort.BAUDRATE_600, SerialPort.BAUDRATE_1200,
-                                                                  SerialPort.BAUDRATE_4800, SerialPort.BAUDRATE_9600,
-                                                                  SerialPort.BAUDRATE_14400, SerialPort.BAUDRATE_19200,
-                                                                  SerialPort.BAUDRATE_38400, SerialPort.BAUDRATE_57600,
-                                                                  SerialPort.BAUDRATE_115200, SerialPort.BAUDRATE_128000,
-                                                                  SerialPort.BAUDRATE_256000);
+    private static final List<Integer> VALID_BAUD = Arrays.asList(BAUDRATE_110,
+                                                                  BAUDRATE_300,
+                                                                  BAUDRATE_600,
+                                                                  BAUDRATE_1200,
+                                                                  BAUDRATE_2400,
+                                                                  BAUDRATE_4800,
+                                                                  BAUDRATE_9600,
+                                                                  BAUDRATE_14400,
+                                                                  BAUDRATE_19200,
+                                                                  BAUDRATE_38400,
+                                                                  BAUDRATE_57600,
+                                                                  BAUDRATE_115200,
+                                                                  BAUDRATE_128000,
+                                                                  BAUDRATE_256000);
 
 
     /**

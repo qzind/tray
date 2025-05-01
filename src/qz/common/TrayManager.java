97f8e345b1153f52ac94ed16dedc1cfb1f5068c0
Tres Finocchiaro <tres.finocchiaro@gmail.com>
2025-05-01 14:07:35 -0400

    Restore old name to function

--- src/qz/common/TrayManager.java
+++ src/qz/common/TrayManager.java
@@ -349,7 +349,7 @@ public class TrayManager {
         componentList.add(aboutDialog);
 
         if (SystemUtilities.isMac()) {
-            MacUtilities.registerUrlHandler();
+            MacUtilities.registerUriHandler();
             MacUtilities.registerAboutDialog(aboutDialog);
             MacUtilities.registerQuitHandler(this);
         }

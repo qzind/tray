49ca537fc0f5c9e3a4d38872f6b95936069ee661
Vzor- <Kyle@Berezin.com>
2025-11-23 21:57:02 -0500

    refactor

--- src/qz/ui/LogStyler.java
+++ src/qz/ui/LogStyler.java
@@ -1,6 +1,5 @@
 package qz.ui;
 
-import org.apache.commons.lang3.ArrayUtils;
 import org.apache.logging.log4j.Level;
 import qz.utils.SystemUtilities;
 
@@ -15,21 +14,27 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class LogStyler {
-    public enum LevelColor {
+    public enum LogColor {
         GRAY(new Color(0x575757), new Color(0x888888)),
         BLUE(new Color(0x0000ff), new Color(0x589dff)),
-        GREEN(new Color(0x2e7d32), new Color(0x47c44d), TokenGroup.TIMESTAMP),
+        GREEN(new Color(0x2e7d32), new Color(0x47c44d)),
         AMBER(new Color(0x805613), new Color(0xf9a825)),
         RED(new Color(0xff0000), new Color(0xeb6261)),
-        PURPLE(new Color(0x9c27b0), new Color(0xce33e8), TokenGroup.CLASS),
-        INDIGO(new Color(0x3f51b5), new Color(0x7589ff), TokenGroup.LINE),
-        DEFAULT(null, null, TokenGroup.MSG);
+        PURPLE(new Color(0x9c27b0), new Color(0xce33e8)),
+        INDIGO(new Color(0x3f51b5), new Color(0x7589ff)),
+        DEFAULT(Color.black, Color.white);
 
-        // Logging Levels have special handling
-        private static final Map<Level, LevelColor> levelColorMap = new HashMap<>();
+        private static final Map<TokenGroup,LogColor> tokenColorMap = new HashMap<>();
+        static {
+            tokenColorMap.put(TokenGroup.TIMESTAMP, GREEN);
+            tokenColorMap.put(TokenGroup.CLASS, PURPLE);
+            tokenColorMap.put(TokenGroup.LINE_NUMBER, INDIGO);
+        }
+
+        private static final Map<Level,LogColor> levelColorMap = new HashMap<>();
         static {
             levelColorMap.put(Level.OFF, DEFAULT);
-            levelColorMap.put(Level.FATAL,RED);
+            levelColorMap.put(Level.FATAL, RED);
             levelColorMap.put(Level.ERROR, RED);
             levelColorMap.put(Level.WARN, AMBER);
             levelColorMap.put(Level.INFO, GREEN);
@@ -38,49 +43,24 @@ public class LogStyler {
             levelColorMap.put(Level.ALL, DEFAULT);
         }
 
-        final SimpleAttributeSet lightAttributeSet;
-        final SimpleAttributeSet darkAttributeSet;
-        final TokenGroup[] tokenGroups;
+        final Color lightThemeColor;
+        final Color darkThemeColor;
 
-        LevelColor(Color lightThemeColor, Color darkThemeColor, TokenGroup ... tokenGroups) {
-            if(lightThemeColor != null) {
-                this.lightAttributeSet = new SimpleAttributeSet();
-                StyleConstants.setForeground(lightAttributeSet, lightThemeColor);
-            } else {
-                this.lightAttributeSet = null;
-            }
-            if(darkThemeColor != null) {
-                this.darkAttributeSet = new SimpleAttributeSet();
-                StyleConstants.setForeground(darkAttributeSet, darkThemeColor);
-            } else {
-                this.darkAttributeSet = null;
-            }
-            this.tokenGroups = tokenGroups;
+        LogColor(Color lightThemeColor, Color darkThemeColor) {
+            this.lightThemeColor = lightThemeColor;
+            this.darkThemeColor = darkThemeColor;
         }
 
-        public SimpleAttributeSet getAttributeSet() {
-            return SystemUtilities.isDarkDesktop() ? darkAttributeSet : lightAttributeSet;
+        public Color getThemeColor() {
+            return SystemUtilities.isDarkDesktop() ? darkThemeColor : lightThemeColor;
         }
 
-        public static SimpleAttributeSet getAttributeSet(TokenGroup tokenGroup, String matchedString) {
-            switch(tokenGroup) {
-                case LEVEL: // parse from levelColorMap
-                    for(Map.Entry<Level, LevelColor> mapEntry : levelColorMap.entrySet()) {
-                        if(matchedString.contains(mapEntry.getKey().name())) {
-                            SimpleAttributeSet set = (SimpleAttributeSet)mapEntry.getValue().getAttributeSet().clone();
-                            StyleConstants.setBold(set, true);
-                            return set;
-                        }
-                    }
-                default:
-                    for(LevelColor levelColor : values()) {
-                        if(ArrayUtils.contains(levelColor.tokenGroups, tokenGroup)) {
-                            return levelColor.getAttributeSet();
-                        }
-                    }
-            }
+        public static LogColor lookup(Level level) {
+            return levelColorMap.get(level);
+        }
 
-            return DEFAULT.getAttributeSet();
+        public static LogColor lookup(TokenGroup tokenGroup) {
+            return tokenColorMap.get(tokenGroup);
         }
     }
 
@@ -89,12 +69,10 @@ public class LogStyler {
      *   e.g. [DEBUG] 2025-11-22T14:58:25,875 @ qz.auth.Certificate:224
      */
     public enum TokenGroup {
-        LEVEL    (Pattern.compile("(\\[[A-Z]+])\\s+")),
-        TIMESTAMP(Pattern.compile("([0-9T:.,-]+)\\s+@\\s+")),
-        CLASS    (Pattern.compile("@\\s+([\\w.$]+):\\d+")),
-        LINE     (Pattern.compile(":(\\d+)$")) ,
-        MSG      (null),
-        WHOLE_STRING(null);
+        LEVEL       (Pattern.compile("(\\[[A-Z]+])\\s+")),
+        TIMESTAMP   (Pattern.compile("([0-9T:.,-]+)\\s+@\\s+")),
+        CLASS       (Pattern.compile("@\\s+([\\w.$]+):\\d+")),
+        LINE_NUMBER (Pattern.compile(":(\\d+)$"));
 
         private final Pattern pattern;
 
@@ -105,23 +83,39 @@ public class LogStyler {
         public Pattern getPattern() {
             return pattern;
         }
+
+        public void configureAttributeSet(SimpleAttributeSet attributeSet, String matchString) {
+            switch(this) {
+                case LEVEL:
+                    if (matchString.length() < 3) return; // should never happen, regex failure guard
+                    matchString = matchString.substring(1, matchString.length() -1); // lose the '[ ]' from the match
+                    StyleConstants.setBold(attributeSet, true);
+                    StyleConstants.setForeground(attributeSet, LogColor.lookup(Level.getLevel(matchString)).getThemeColor());
+                    break;
+                default:
+                    StyleConstants.setBold(attributeSet, false);
+                    StyleConstants.setForeground(attributeSet, LogColor.lookup(this).getThemeColor());
+            }
+        }
     }
 
     public static void appendStyledText(StyledDocument doc, String text) {
-        int offset = doc.getLength();
-        append(doc, text);
-        String logLine = text.substring(0, text.indexOf('\n'));
+        synchronized(doc) {
+            int offset = doc.getLength();
+            append(doc, text);
+            String logLine = text.substring(0, text.indexOf('\n'));
+            SimpleAttributeSet attr = new SimpleAttributeSet();
 
-        for(TokenGroup tokenGroup : TokenGroup.values()) {
-            if(tokenGroup.getPattern() == null) continue;
-            Matcher tokens = tokenGroup.getPattern().matcher(logLine);
-            if(tokens.find()) {
-                int startIndex = offset + tokens.start(1);
-                int endIndex = offset + tokens.end(1) + 1;
-                String matchedString = tokens.group(1);
-                SimpleAttributeSet attr = LevelColor.getAttributeSet(tokenGroup, matchedString);
-                if (attr != null) {
-                    doc.setCharacterAttributes(startIndex, endIndex - startIndex, attr, false);
+            for(TokenGroup tokenGroup : TokenGroup.values()) {
+                if (tokenGroup.getPattern() == null) continue;
+                Matcher tokens = tokenGroup.getPattern().matcher(logLine);
+                if (tokens.find()) {
+                    int startIndex = offset + tokens.start(1);
+                    int endIndex = offset + tokens.end(1) + 1;
+                    tokenGroup.configureAttributeSet(attr, tokens.group(1));
+                    if (attr != null) {
+                        doc.setCharacterAttributes(startIndex, endIndex - startIndex, attr, false);
+                    }
                 }
             }
         }

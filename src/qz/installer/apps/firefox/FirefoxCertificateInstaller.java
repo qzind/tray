088b48c3c42880c9189a937b4a64a25c74b9b7ea
Tres Finocchiaro <tres.finocchiaro@gmail.com>
2026-01-21 16:58:31 -0500

    Initial Firefox code cleanup (WIP)

--- src/qz/installer/apps/firefox/FirefoxCertificateInstaller.java
+++ src/qz/installer/apps/firefox/FirefoxCertificateInstaller.java
@@ -12,17 +12,21 @@ package qz.installer.apps.firefox;
 
 import com.github.zafarkhaja.semver.Version;
 import com.sun.jna.platform.win32.WinReg;
+import org.codehaus.jettison.json.JSONArray;
 import org.codehaus.jettison.json.JSONException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.codehaus.jettison.json.JSONObject;
 import qz.common.Constants;
 import qz.installer.Installer;
+import qz.installer.LinuxInstaller;
 import qz.installer.apps.ConflictingPolicyException;
 import qz.installer.apps.firefox.legacy.LegacyFirefoxCertificateInstaller;
 import qz.installer.certificate.CertificateManager;
 import qz.installer.apps.firefox.locator.AppAlias;
 import qz.installer.apps.firefox.locator.AppInfo;
 import qz.installer.apps.firefox.locator.AppLocator;
+import qz.installer.certificate.KeyPairWrapper;
 import qz.utils.JsonWriter;
 import qz.utils.ShellUtilities;
 import qz.utils.SystemUtilities;
@@ -37,6 +41,8 @@ import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.Base64;
 
+import static qz.utils.FileUtilities.SHARED_DIR;
+
 /**
  * Installs the Firefox Policy file via Enterprise Policy, Distribution Policy file or AutoConfig, depending on OS & version
  */
@@ -48,22 +54,79 @@ public class FirefoxCertificateInstaller {
      * 3rd-party/clones may adopt Enterprise Policy support under
      * different version numbers, adapt as needed.
      */
-    private static final Version WINDOWS_POLICY_VERSION = Version.valueOf("62.0.0");
-    private static final Version MAC_POLICY_VERSION = Version.valueOf("63.0.0");
-    private static final Version LINUX_POLICY_VERSION = Version.valueOf("65.0.0");
-    public static final Version FIREFOX_RESTART_VERSION = Version.valueOf("60.0.0");
+    private static final Version WINDOWS_POLICY_VERSION = Version.parse("62.0.0");
+    private static final Version MAC_POLICY_VERSION = Version.parse("63.0.0");
+    private static final Version LINUX_POLICY_VERSION = Version.parse("65.0.0");
+    public static final Version FIREFOX_RESTART_VERSION = Version.parse("60.0.0");
 
-    public static final String LINUX_GLOBAL_POLICY_LOCATION = "/etc/firefox/policies/policies.json";
+    public static final String FIREFOX_GLOBAL_POLICY_DIRECTORY = "/etc/firefox/policies";
+    public static final String FIREFOX_GLOBAL_POLICY_LOCATION = String.format("%s/policies.json", FIREFOX_GLOBAL_POLICY_DIRECTORY);
+    public static final String FIREFOX_SNAP_CERT_LOCATION = String.format("%s/%s%s", FIREFOX_GLOBAL_POLICY_DIRECTORY, Constants.PROPS_FILE, CertificateManager.DEFAULT_CERTIFICATE_EXTENSION); // See https://github.com/mozilla/policy-templates/issues/936
     public static final String LINUX_SNAP_CERT_LOCATION = "/etc/firefox/policies/" + Constants.PROPS_FILE + CertificateManager.DEFAULT_CERTIFICATE_EXTENSION; // See https://github.com/mozilla/policy-templates/issues/936
-    public static final String LINUX_GLOBAL_CERT_LOCATION = "/usr/lib/mozilla/certificates/" + Constants.PROPS_FILE + CertificateManager.DEFAULT_CERTIFICATE_EXTENSION;
-    private static String DISTRIBUTION_ENTERPRISE_ROOT_POLICY = "{ \"policies\": { \"Certificates\": { \"ImportEnterpriseRoots\": true } } }";
-    private static String DISTRIBUTION_INSTALL_CERT_POLICY = "{ \"policies\": { \"Certificates\": { \"Install\": [ \"" + Constants.PROPS_FILE + CertificateManager.DEFAULT_CERTIFICATE_EXTENSION + "\", \"" + LINUX_SNAP_CERT_LOCATION + "\" ] } } }";
-    private static String DISTRIBUTION_REMOVE_CERT_POLICY = "{ \"policies\": { \"Certificates\": { \"Install\": [ \"/opt/" + Constants.PROPS_FILE +  "/auth/root-ca.crt\"] } } }";
+    public static final String LINUX_GLOBAL_CERT_LOCATION = String.format("/usr/lib/mozilla/certificates/%s%s", Constants.PROPS_FILE, CertificateManager.DEFAULT_CERTIFICATE_EXTENSION);
+
+    private static final JSONObject DISTRIBUTION_ENTERPRISE_ROOT_POLICY;
+    private static final JSONObject DISTRIBUTION_INSTALL_CERT_POLICY;
+    private static final JSONObject DISTRIBUTION_REMOVE_CERT_POLICY;
+
+    static {
+        try {
+            DISTRIBUTION_ENTERPRISE_ROOT_POLICY = new JSONObject()
+                    .put("policies", new JSONObject()
+                            .put("Certificates", new JSONObject()
+                                    .put("ImportEnterpriseRoots", true)));
+
+            // Note: "qz-tray.crt" is a copy of "root-ca.crt", renamed to avoid collision
+            String certName = String.format("%s%s", KeyPairWrapper.getAlias(KeyPairWrapper.Type.SSL), CertificateManager.DEFAULT_CERTIFICATE_EXTENSION);
+
+            //String caCertName = String.format("%s%s", KeyPairWrapper.getAlias(KeyPairWrapper.Type.CA), CertificateManager.DEFAULT_CERTIFICATE_EXTENSION);
+            //Path qzRootCertPath =  Paths.get(SHARED_DIR.toString(), "ssl", caCertName);
+
+            String linuxSnapCertLocation = String.format("%s/%s", FIREFOX_GLOBAL_POLICY_DIRECTORY, certName); // See https://github.com/mozilla/policy-templates/issues/936
+
+            DISTRIBUTION_INSTALL_CERT_POLICY = new JSONObject()
+                    .put("policies", new JSONObject()
+                            .put("Certificates", new JSONObject()
+                                    .put("Install", new JSONArray()
+                                            .put(certName)
+                                            .put(linuxSnapCertLocation))));
+
+
+            // Policies to clean up from previous versions
+            Path oldCertPath = Paths.get( "/opt/",Constants.PROPS_FILE, "auth", "root-ca.crt");
+            DISTRIBUTION_REMOVE_CERT_POLICY = new JSONObject()
+                    .put("policies", new JSONObject()
+                            .put("Certificates", new JSONObject()
+                                    .put("Install", new JSONArray()
+                                            .put(oldCertPath))));
+        } catch(JSONException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    static {
+        String DISTRIBUTION_ENTERPRISE_ROOT_POLICY_OLD = "{ \"policies\": { \"Certificates\": { \"ImportEnterpriseRoots\": true } } }";
+        String DISTRIBUTION_INSTALL_CERT_POLICY_OLD = "{ \"policies\": { \"Certificates\": { \"Install\": [ \"" + Constants.PROPS_FILE + CertificateManager.DEFAULT_CERTIFICATE_EXTENSION + "\", \"" + LINUX_SNAP_CERT_LOCATION + "\" ] } } }";
+        String DISTRIBUTION_REMOVE_CERT_POLICY_OLD = "{ \"policies\": { \"Certificates\": { \"Install\": [ \"/opt/" + Constants.PROPS_FILE +  "/auth/root-ca.crt\"] } } }";
+
+        System.out.println(DISTRIBUTION_ENTERPRISE_ROOT_POLICY_OLD);
+        System.out.println(DISTRIBUTION_ENTERPRISE_ROOT_POLICY);
+
+        System.out.println(DISTRIBUTION_INSTALL_CERT_POLICY_OLD);
+        System.out.println(DISTRIBUTION_INSTALL_CERT_POLICY);
+
+        System.out.println(DISTRIBUTION_REMOVE_CERT_POLICY_OLD);
+        System.out.println(DISTRIBUTION_REMOVE_CERT_POLICY);
+    }
 
     public static final String DISTRIBUTION_POLICY_LOCATION = "distribution/policies.json";
     public static final String DISTRIBUTION_MAC_POLICY_LOCATION = "Contents/Resources/" + DISTRIBUTION_POLICY_LOCATION;
 
     public static final String POLICY_AUDIT_MESSAGE = "Enterprise policy installed by " + Constants.ABOUT_TITLE + " on " + SystemUtilities.timeStamp();
+
+    public static void main(String ... args) {
+        // just looking at static fields for now...
+    }
 
     public static void install(X509Certificate cert, String ... hostNames) {
         // Blindly install Firefox enterprise policies to the system (macOS, Windows)
@@ -123,7 +186,7 @@ public class FirefoxCertificateInstaller {
                     try {
                         File policy = appInfo.getPath().resolve(DISTRIBUTION_POLICY_LOCATION).toFile();
                         if(policy.exists()) {
-                            JsonWriter.write(appInfo.getPath().resolve(DISTRIBUTION_POLICY_LOCATION).toString(), DISTRIBUTION_INSTALL_CERT_POLICY, false, true);
+                            JsonWriter.write(appInfo.getPath().resolve(DISTRIBUTION_POLICY_LOCATION).toString(), DISTRIBUTION_INSTALL_CERT_POLICY.toString(), false, true);
                         }
                     } catch(IOException | JSONException e) {
                         log.warn("Unable to remove Firefox policy for {}", appInfo, e);
@@ -142,11 +205,11 @@ public class FirefoxCertificateInstaller {
             return false;
         }
         if(SystemUtilities.isWindows()) {
-            return appInfo.getVersion().greaterThanOrEqualTo(WINDOWS_POLICY_VERSION);
+            return appInfo.getVersion().isHigherThanOrEquivalentTo(WINDOWS_POLICY_VERSION);
         } else if (SystemUtilities.isMac()) {
-            return appInfo.getVersion().greaterThanOrEqualTo(MAC_POLICY_VERSION);
+            return appInfo.getVersion().isHigherThanOrEquivalentTo(MAC_POLICY_VERSION);
         } else {
-            return appInfo.getVersion().greaterThanOrEqualTo(LINUX_POLICY_VERSION);
+            return appInfo.getVersion().isHigherThanOrEquivalentTo(LINUX_POLICY_VERSION);
         }
     }
 
@@ -174,29 +237,32 @@ public class FirefoxCertificateInstaller {
                 }
                 throw new ConflictingPolicyException(String.format("%s enterprise policy conflict at %s: %s", alias.getName(), policyLocation + alias.getBundleId(), foundPolicy));
             }
-        } else {
+        } /*
+        else {
             // Linux alternate policy not yet supported
         }
+        */
         return false;
     }
 
     /**
      * Install policy to distribution/policies.json
      */
+    @SuppressWarnings("ResultOfMethodCallIgnored")
     public static boolean installDistributionPolicy(AppInfo app, X509Certificate cert) {
         Path jsonPath = app.getPath().resolve(SystemUtilities.isMac() ? DISTRIBUTION_MAC_POLICY_LOCATION:DISTRIBUTION_POLICY_LOCATION);
-        String jsonPolicy = SystemUtilities.isWindows() || SystemUtilities.isMac() ? DISTRIBUTION_ENTERPRISE_ROOT_POLICY:DISTRIBUTION_INSTALL_CERT_POLICY;
+        JSONObject jsonPolicy = SystemUtilities.isWindows() || SystemUtilities.isMac() ? DISTRIBUTION_ENTERPRISE_ROOT_POLICY:DISTRIBUTION_INSTALL_CERT_POLICY;
 
         // Special handling for snaps
         if(app.getPath().toString().startsWith("/snap")) {
-            log.info("Snap detected, installing policy file to global location instead: {}", LINUX_GLOBAL_POLICY_LOCATION);
-            jsonPath = Paths.get(LINUX_GLOBAL_POLICY_LOCATION);
+            log.info("Snap detected, installing policy file to global location instead: {}", FIREFOX_GLOBAL_POLICY_LOCATION);
+            jsonPath = Paths.get(FIREFOX_GLOBAL_POLICY_LOCATION);
         }
 
         try {
             if(jsonPolicy.equals(DISTRIBUTION_INSTALL_CERT_POLICY)) {
                 // Linux lacks the concept of "enterprise roots", we'll write it to a known location instead
-                writeCertFile(cert, LINUX_SNAP_CERT_LOCATION); // so that the snap can read from it
+                writeCertFile(cert, FIREFOX_SNAP_CERT_LOCATION); // so that the snap can read from it
                 writeCertFile(cert, LINUX_GLOBAL_CERT_LOCATION); // default location for non-snaps
             }
 
@@ -210,14 +276,13 @@ public class FirefoxCertificateInstaller {
 
             if(jsonPolicy.equals(DISTRIBUTION_INSTALL_CERT_POLICY)) {
                 // Delete previous policy
-                JsonWriter.write(jsonPath.toString(), DISTRIBUTION_REMOVE_CERT_POLICY, false, true);
+                JsonWriter.write(jsonPath.toString(), DISTRIBUTION_REMOVE_CERT_POLICY.toString(), false, true);
             }
 
-            JsonWriter.write(jsonPath.toString(), jsonPolicy, false, false);
+            JsonWriter.write(jsonPath.toString(), jsonPolicy.toString(), false, false);
 
             // Make sure ew can read
-            jsonFile.setReadable(true, false);
-            return true;
+            return jsonFile.setReadable(true, false);
         } catch(JSONException | IOException e) {
             log.warn("Could not install distribution policy {} to {}", jsonPolicy, jsonPath.toString(), e);
         }
@@ -241,18 +306,20 @@ public class FirefoxCertificateInstaller {
         return false;
     }
 
+    @SuppressWarnings("UnusedReturnValue")
     public static boolean issueRestartWarning(ArrayList<Path> runningPaths, AppInfo appInfo) {
         boolean firefoxIsRunning = runningPaths.contains(appInfo.getExePath());
 
         // Edge case for detecting if snap is running, since we can't compare the exact path easily
         for(Path runningPath : runningPaths) {
-            if(runningPath.startsWith("/snap/")) {
+            if (runningPath.startsWith("/snap/")) {
                 firefoxIsRunning = true;
+                break;
             }
         }
 
         if (firefoxIsRunning) {
-            if (appInfo.getVersion().greaterThanOrEqualTo(FirefoxCertificateInstaller.FIREFOX_RESTART_VERSION)) {
+            if (appInfo.getVersion().isHigherThanOrEquivalentTo(FirefoxCertificateInstaller.FIREFOX_RESTART_VERSION)) {
                 try {
                     Installer.getInstance().spawn(appInfo.getExePath().toString(), "-private", "about:restartrequired");
                     return true;
@@ -265,6 +332,8 @@ public class FirefoxCertificateInstaller {
         return false;
     }
 
+
+    @SuppressWarnings("ResultOfMethodCallIgnored")
     private static void writeCertFile(X509Certificate cert, String location) throws IOException {
         File certFile = new File(location);
 

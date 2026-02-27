b9fb156d384bc9db110f0ed96df6840d32f62375
Tres Finocchiaro <tres.finocchiaro@gmail.com>
2026-02-27 14:18:37 -0500

    More user-install fixes

--- src/qz/installer/shortcut/LinuxShortcutCreator.java
+++ src/qz/installer/shortcut/LinuxShortcutCreator.java
@@ -12,6 +12,7 @@ package qz.installer.shortcut;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import org.apache.logging.log4j.LogManager;
@@ -19,38 +20,44 @@ import org.apache.logging.log4j.Logger;
 import qz.installer.LinuxInstaller;
 import qz.utils.SystemUtilities;
 
+import static qz.installer.LinuxInstaller.*;
+
 /**
  * @author Tres Finocchiaro
  */
 class LinuxShortcutCreator extends ShortcutCreator {
-
     private static final Logger log = LogManager.getLogger(LinuxShortcutCreator.class);
-    private static String DESKTOP = System.getProperty("user.home") + "/Desktop/";
+    private final Path desktopDir;
+    private final String appLauncherDir;
+    private final String startupDir;
+
+    public LinuxShortcutCreator() {
+        boolean systemWide = SystemUtilities.isInstalledSystemWide();
+        appLauncherDir =  systemWide ? SYSTEM_APP_LAUNCHER : String.format(USER_APP_LAUNCHER, System.getProperty("user.home"));
+        startupDir = systemWide ? SYSTEM_STARTUP_DIR : String.format(LinuxInstaller.USER_STARTUP_DIR, System.getProperty("user.home"));
+        desktopDir = Paths.get(System.getProperty("user.home"), "Desktop");
+    }
 
     public boolean canAutoStart() {
         if(SystemUtilities.isInstalled()) {
             return Files.exists(Paths.get(
-                    SystemUtilities.isInstalledSystemWide()?
-                            LinuxInstaller.SYSTEM_STARTUP_DIR:
-                            LinuxInstaller.USER_STARTUP_DIR,
+                    startupDir,
                     LinuxInstaller.SHORTCUT_NAME));
         }
         return false;
     }
     public void createDesktopShortcut() {
         if(SystemUtilities.isInstalled()) {
-            copyShortcut(SystemUtilities.isInstalledSystemWide()?
-                                 LinuxInstaller.SYSTEM_APP_LAUNCHER:
-                                 LinuxInstaller.USER_APP_LAUNCHER,
-                         DESKTOP);
+            Path shortcut = Paths.get(appLauncherDir, SHORTCUT_NAME);
+            copyShortcut(shortcut, desktopDir.resolve(SHORTCUT_NAME));
         } else {
             log.warn("Skipping creation of Desktop icon, we don't appear to be installed");
         }
     }
 
-    private static void copyShortcut(String source, String target) {
+    private static void copyShortcut(Path source, Path target) {
         try {
-            Files.copy(Paths.get(source), Paths.get(target));
+            Files.copy(source, target);
         } catch(IOException e) {
             log.warn("Error creating shortcut {}", target, e);
         }

3c8a129f6776f7e167b0cd348209b2a7bac4915a
Tres Finocchiaro <tres.finocchiaro@gmail.com>
2026-03-28 15:35:52 -0400

    Workaround quirks with logname, sh vs bash

--- test/qz/installer/provision/resources/script3.sh
+++ test/qz/installer/provision/resources/script3.sh
@@ -1,12 +1,7 @@
 shell=$(ps -p $$ -oargs=|awk '{print $1}')
 date=$(date "+%F %T")
 script=$(basename "$0")
-
-if [[ "$OSTYPE" == "darwin"* ]]; then
-  user="$(eval echo ~$USER)"
-else
-  user="$(eval echo ~$(logname))"
-fi
+user=$(eval echo "~$(id -un)")
 
 echo "$date Successful provisioning test from '$shell': $script" >> "$user/Desktop/provision.log"
 chmod 664 "$user/Desktop/provision.log"

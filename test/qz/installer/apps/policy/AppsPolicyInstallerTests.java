448901e64aac682a188f4234de977006240ae3e9
Vzor- <Kyle@Berezin.com>
2026-03-31 01:33:47 -0400

    hasta la vista AppsPolicyInstallerTests

--- test/qz/installer/apps/policy/AppsPolicyInstallerTests.java
+++
@@ -1,9 +0,0 @@
-package qz.installer.apps.policy;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-public class AppsPolicyInstallerTests {
-    private static final Logger log = LogManager.getLogger(AppsPolicyInstallerTests.class);
-    //todo retroactive dedupe test
-}

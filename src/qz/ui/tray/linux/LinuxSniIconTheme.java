ca6af90c48fed1f0278238165d476d2ff1ec569e
Hiram K <hiram.kamau@outlook.com>
2026-06-04 13:38:47 +0300

    Add SNI icon theme metadata

--- src/qz/ui/tray/linux/LinuxSniIconTheme.java
+++ src/qz/ui/tray/linux/LinuxSniIconTheme.java
@@ -4,6 +4,7 @@ import qz.utils.FileUtilities;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
@@ -17,6 +18,7 @@ class LinuxSniIconTheme {
     static String prepare() throws IOException {
         Path themePath = getThemePath();
 
+        writeThemeIndex(themePath);
         for(int size : ICON_SIZES) {
             copyIcon(size, themePath);
         }
@@ -28,6 +30,34 @@ class LinuxSniIconTheme {
         return FileUtilities.TEMP_DIR != null
                 ? FileUtilities.TEMP_DIR.resolve("sni-icons")
                 : Files.createTempDirectory("qz_sni_icons_");
+    }
+
+    private static void writeThemeIndex(Path themePath) throws IOException {
+        Path indexPath = themePath.resolve("hicolor").resolve("index.theme");
+        StringBuilder directories = new StringBuilder();
+        StringBuilder sections = new StringBuilder();
+
+        for(int size : ICON_SIZES) {
+            if(directories.length() > 0) {
+                directories.append(',');
+            }
+            directories.append(size).append('x').append(size).append("/apps");
+            sections.append(System.lineSeparator())
+                    .append('[').append(size).append('x').append(size).append("/apps]")
+                    .append(System.lineSeparator())
+                    .append("Size=").append(size).append(System.lineSeparator())
+                    .append("Context=Applications").append(System.lineSeparator())
+                    .append("Type=Fixed").append(System.lineSeparator());
+        }
+
+        String index = "[Icon Theme]" + System.lineSeparator()
+                + "Name=QZ Tray" + System.lineSeparator()
+                + "Comment=Temporary QZ Tray StatusNotifier icons" + System.lineSeparator()
+                + "Directories=" + directories + System.lineSeparator()
+                + sections;
+
+        Files.createDirectories(indexPath.getParent());
+        Files.writeString(indexPath, index, StandardCharsets.UTF_8);
     }
 
     private static void copyIcon(int size, Path themePath) throws IOException {

c0da996371b22bf167350dc5ae9f72c2885e7e6d
bratpeki <pkatic2003@gmail.com>
2026-06-08 22:31:56 +0200

    Switched to getResourceAsStream

--- test/qz/integration/services/Jbig2Tests.java
+++ test/qz/integration/services/Jbig2Tests.java
@@ -1,12 +1,11 @@
 package qz.integration.services;
 
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.nio.file.Path;
 import javax.imageio.ImageIO;
 import javax.imageio.stream.ImageInputStream;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import java.io.InputStream;
 
 public class Jbig2Tests {
 
@@ -15,53 +14,38 @@ public class Jbig2Tests {
 
 		ImageIO.scanForPlugins();
 
-		Path projectRoot = Path.of(System.getProperty("user.dir"));
+		String assetPath = "qz/integration/services/assets/jbig2-cameraman.jb2";
+		String truthPath = "qz/integration/services/assets/jbig2-cameraman.png";
 
-		Path assetsFolder = projectRoot
-			.resolve("test")
-			.resolve("qz")
-			.resolve("integration")
-			.resolve("services")
-			.resolve("assets");
+		try (InputStream jb2Stream = getClass().getClassLoader().getResourceAsStream(assetPath);
+			 InputStream truthStream = getClass().getClassLoader().getResourceAsStream(truthPath)) {
 
-		File jb2File = assetsFolder.resolve("jbig2-cameraman.jb2").toFile();
-		Assert.assertTrue(jb2File.exists(), "Can't find JB2 at %s!".formatted(jb2File.getAbsolutePath()));
+			Assert.assertNotNull(jb2Stream, "Could not find asset on classpath: " + assetPath);
+			Assert.assertNotNull(truthStream, "Could not find truth file on classpath: " + truthPath);
 
-		try (ImageInputStream iis = ImageIO.createImageInputStream(jb2File)) {
+			try (javax.imageio.stream.ImageInputStream iis = ImageIO.createImageInputStream(jb2Stream)) {
+				org.apache.pdfbox.jbig2.JBIG2ImageReader reader = new org.apache.pdfbox.jbig2.JBIG2ImageReader(
+					new org.apache.pdfbox.jbig2.JBIG2ImageReaderSpi()
+				);
 
-			Assert.assertNotNull(iis, "Failed to create ImageInputStream from file!");
+				try {
+					reader.setInput(iis, false, false);
+					BufferedImage decoded = reader.read(0);
+					Assert.assertNotNull(decoded, "Decoded image is null!");
 
-			org.apache.pdfbox.jbig2.JBIG2ImageReader reader = new org.apache.pdfbox.jbig2.JBIG2ImageReader(
-				new org.apache.pdfbox.jbig2.JBIG2ImageReaderSpi()
-			);
+					BufferedImage truth = ImageIO.read(truthStream);
+					Assert.assertNotNull(truth, "Truth image failed to load!");
 
-			try {
-
-				reader.setInput(iis, false, false);
-
-				BufferedImage decoded = reader.read(0);
-				Assert.assertNotNull(decoded, "Decoded image is null!");
-
-				File truthFile = assetsFolder.resolve("jbig2-cameraman.png").toFile();
-				Assert.assertTrue(truthFile.exists(), "File does not exist: " + truthFile.getAbsolutePath());
-
-				BufferedImage truth = ImageIO.read(truthFile);
-				Assert.assertNotNull(truth);
-
-				Assert.assertEquals(decoded.getWidth(), truth.getWidth());
-				Assert.assertEquals(decoded.getHeight(), truth.getHeight());
-
-				for (int y = 0; y < decoded.getHeight(); y++) {
-					for (int x = 0; x < decoded.getWidth(); x++) {
-						Assert.assertEquals(
-							decoded.getRGB(x, y),
-							truth.getRGB(x, y),
-							"Lossless failure at pixel %d,%d!".formatted(x,y)
-						);
+					for (int y = 0; y < decoded.getHeight(); y++) {
+						for (int x = 0; x < decoded.getWidth(); x++) {
+							Assert.assertEquals(decoded.getRGB(x, y), truth.getRGB(x, y),
+								"Mismatch at %d,%d".formatted(x, y));
+						}
 					}
 				}
+				finally { reader.dispose(); }
 
-			} finally { reader.dispose(); }
+			}
 
 		}
 

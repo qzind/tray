bc3c87e1dbe560349f4407d3f1fecdcb6d095d62
Tres Finocchiaro <tres.finocchiaro@gmail.com>
2026-06-15 13:54:48 -0400

    Cleanup after merge

--- test/qz/integration/services/SecuritySpiTests.java
+++ test/qz/integration/services/SecuritySpiTests.java
@@ -2,17 +2,14 @@ package qz.integration.services;
 
 import java.security.Provider;
 import java.security.Security;
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
-import org.bouncycastle.pqc.jcajce.provider.BouncyCastlePQCProvider;
 import org.testng.Assert;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class SecuritySpiTests {
 
 	@DataProvider(name = "providers")
-	public Object[][] providers() throws ClassNotFoundException {
+	public Object[][] providers() {
 		return new Object[][] {
 			{ "BC", "org.bouncycastle.jce.provider.BouncyCastleProvider" },
 			{ "BCPQC", "org.bouncycastle.pqc.jcajce.provider.BouncyCastlePQCProvider" },

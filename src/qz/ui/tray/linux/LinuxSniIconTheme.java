996284cbe32cd88e87830bda2e31b167ac67108c
Hiram K <hiram.kamau@outlook.com>
2026-07-03 14:38:26 +0300

    Try out SVG path for Linux notifications

--- src/qz/ui/tray/linux/LinuxSniIconTheme.java
+++ src/qz/ui/tray/linux/LinuxSniIconTheme.java
@@ -42,6 +42,15 @@ class LinuxSniIconTheme {
                 .toString();
     }
 
+    static String getSvgIconPath(String themePath) {
+        return Path.of(themePath)
+                .resolve("hicolor")
+                .resolve("scalable")
+                .resolve("apps")
+                .resolve(SYMBOLIC_ICON_NAME + ".svg")
+                .toString();
+    }
+
     private static Path getThemePath() throws IOException {
         return FileUtilities.TEMP_DIR != null
                 ? FileUtilities.TEMP_DIR.resolve("sni-icons")

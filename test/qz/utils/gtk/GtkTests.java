20478eea638f71eedd2a2dec2c99d6c1554bc92d
Tres Finocchiaro <tres.finocchiaro@gmail.com>
2026-08-26 19:17:30 -0400

    Add non-headless coverage

--- test/qz/utils/gtk/GtkTests.java
+++ test/qz/utils/gtk/GtkTests.java
@@ -6,6 +6,8 @@ import org.testng.SkipException;
 import org.testng.annotations.Test;
 import qz.utils.SystemUtilities;
 
+import java.awt.*;
+
 public class GtkTests {
     @Test
     public void testGtkAvailability() {
@@ -14,8 +16,12 @@ public class GtkTests {
             case MAC:
                 throw new SkipException("GTK is not needed on macOS or Windows");
         }
-        Gtk GTK_INSTANCE = Gtk.getInstance(false);
+        boolean init = !GraphicsEnvironment.isHeadless();
+        Gtk GTK_INSTANCE = Gtk.getInstance(init);
         Assert.assertNotNull(GTK_INSTANCE);
         Assert.assertNotEquals(GTK_INSTANCE.getVersion(), Version.of(0));
+        if(init) {
+            Assert.assertTrue(GTK_INSTANCE.getScaleFactor() > 0);
+        }
     }
 }

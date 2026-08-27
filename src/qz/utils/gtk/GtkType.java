3cdbedcfe99f373285a5a375b556e4fe6a8c876d
Tres Finocchiaro <tres.finocchiaro@gmail.com>
2026-08-27 16:52:04 -0400

    Better Gtk detection (#1502)
    Improve Gtk detection via JNA
    
    Co-authored-by: IdelsTak <contact.kamau@gmail.com>

---
+++ src/qz/utils/gtk/GtkType.java
@@ -0,0 +1,15 @@
+package qz.utils.gtk;
+
+enum GtkType {
+    GTK4("gtk-4", Gtk4.class),
+    GTK3("gtk-3", Gtk3.class),
+    GTK2("gtk-x11-2.0", Gtk2.class);
+
+    final String lib;
+    final Class<? extends Gtk> type;
+
+    GtkType(String lib, Class<? extends Gtk> type) {
+        this.lib = lib;
+        this.type = type;
+    }
+}

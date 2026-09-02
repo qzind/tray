1e84e6c43bde4a5641998e3d2e50f06c07f46eda
Tres Finocchiaro <tres.finocchiaro@gmail.com>
2026-09-02 14:58:40 -0400

    Cleanup logs for when color-scheme isn't supported

--- src/qz/utils/linux/DeUtils.java
+++ src/qz/utils/linux/DeUtils.java
@@ -6,11 +6,16 @@ import qz.utils.ShellUtilities;
 
 public class DeUtils {
     private static final Logger log = LogManager.getLogger(DeUtils.class);
+    private static boolean darkDesktopDetectionFailed = false;
 
     /**
      * Uses a live desktop-environment-specific technique determine if the Desktop is "dark mode".
      */
     public static boolean isDarkDesktop() {
+        if(darkDesktopDetectionFailed) {
+            return false;
+        }
+
         String colorScheme = ShellUtilities.executeRawSilently("gdbus", "call", "--session",
                                           "--dest", "org.freedesktop.portal.Desktop",
                                           "--object-path", "/org/freedesktop/portal/desktop",
@@ -19,12 +24,17 @@ public class DeUtils {
 
         try {
             // (<uint32 0>,)
-            String isolated = colorScheme.trim().split("\\s+")[1].replaceAll("[>,)]+$", "");
-            // 0: No preference, 1: Prefer dark, 2: Prefer light
-            return Integer.parseInt(isolated) == 1;
-        } catch(NumberFormatException | IndexOutOfBoundsException nfe) {
-            log.warn("Unable to parse color scheme from '{}'", colorScheme, nfe);
+            String[] parts = colorScheme.trim().split("\\s+");
+            if(parts.length > 1) {
+                String isolated = parts[1].replaceAll("[>,)]+$", "");
+                // 0 = No preference; 1 = Prefer dark; 2 = Prefer light
+                return Integer.parseInt(isolated) == 1;
+            }
+        } catch(NumberFormatException nfe) {
+            log.warn("Unable to parse color scheme from '{}': {}", colorScheme, nfe.getMessage());
         }
+        darkDesktopDetectionFailed = true;
+        log.warn("Can't detect color scheme; We'll remaining on light mode until the app is restarted");
         return false;
     }
 }

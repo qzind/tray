9dc7266d383a26ac84f8ca4deefa24803e9c4d79
Hiram K <hiram.kamau@outlook.com>
2026-09-17 09:25:20 +0300

    Test representative IconCache resources

--- test/qz/ui/component/IconCacheTests.java
+++ test/qz/ui/component/IconCacheTests.java
@@ -4,6 +4,8 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import static qz.ui.component.IconCache.Icon.*;
+
 public class IconCacheTests {
     private IconCache iconCache;
 
@@ -15,5 +17,14 @@ public class IconCacheTests {
     @Test
     public void testCacheBuilds() {
         Assert.assertNotNull(iconCache);
+    }
+
+    @Test
+    public void testRepresentativeResourcesLoad() {
+        Assert.assertNotNull(iconCache.getImage(LOGO_ICON), "Logo icon should load");
+        Assert.assertNotNull(iconCache.getImage(DEFAULT_ICON), "Tray icon should load");
+        Assert.assertNotNull(iconCache.getImage(DEFAULT_MASK_ICON), "Tray mask icon should load");
+        Assert.assertNotNull(iconCache.getImage(ABOUT_ICON), "Menu icon should load");
+        Assert.assertNotNull(iconCache.getImage(TRUST_VERIFIED_ICON), "Dialog icon should load");
     }
 }

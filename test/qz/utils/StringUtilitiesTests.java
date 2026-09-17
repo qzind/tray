76c504a0f9d90ff8e1f4cdd39770e0adb3ef44bd
Tres Finocchiaro <tres.finocchiaro@gmail.com>
2026-09-17 16:15:35 -0400

    Fix unit tests

--- test/qz/utils/StringUtilitiesTests.java
+++ test/qz/utils/StringUtilitiesTests.java
@@ -8,15 +8,15 @@ import java.nio.charset.StandardCharsets;
 public class StringUtilitiesTests {
     @Test
     public void testLegacyEncoding() {
-        if(SystemUtilities.isWindows()) {
+        if(!SystemUtilities.isWindows()) {
             Assert.assertEquals(
                     StringUtilities.getCharset("legacy"),
                     StandardCharsets.UTF_8);
+        } else {
+            // Verify that encoding "legacy" returns "windows-1252" or similar 8-bit encoding
+            Assert.assertNotEquals(
+                    StringUtilities.getCharset("legacy"),
+                    StandardCharsets.UTF_8);
         }
-
-        // Verify that encoding "legacy" returns "windows-1252" or similar 8-bit encoding
-        Assert.assertNotEquals(
-                StringUtilities.getCharset("legacy"),
-                StandardCharsets.UTF_8);
     }
 }
